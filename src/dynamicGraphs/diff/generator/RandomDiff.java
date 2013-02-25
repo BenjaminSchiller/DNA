f11f71a2ac0cd9103f1c83d34fb5be7e187f0887
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-02-25 15:50:24 +0100

    adds random graph / diff generators extending the abstract classes

--- src/dynamicGraphs/diff/generator/RandomDiff.java
+++ src/dynamicGraphs/diff/generator/RandomDiff.java
@@ -1,35 +1,45 @@
 package dynamicGraphs.diff.generator;
 
+import java.util.ArrayList;
+
 import dynamicGraphs.diff.Diff;
 import dynamicGraphs.graph.Edge;
 import dynamicGraphs.graph.Graph;
 import dynamicGraphs.util.Rand;
 
-public class RandomDiff {
-	public static Diff generate(Graph g, int add, int remove, boolean undirected) {
-		return RandomDiff.generate(g, add, remove, undirected,
-				g.getTimestamp(), g.getTimestamp() + 1);
+public class RandomDiff extends DiffGenerator {
+	public RandomDiff(int add, int remove, boolean undirected) {
+		super("RandomDiff-" + add + "-" + remove + "-" + undirected);
+		this.add = add;
+		this.remove = remove;
+		this.undirected = undirected;
 	}
 
-	// TODO only remove existing edges
-	// TODO only add missing edges
-	public static Diff generate(Graph g, int add, int remove,
-			boolean undirected, long from, long to) {
-		Diff d = new Diff(g.getNodes().length, from, to);
+	private int add;
+
+	private int remove;
+
+	private boolean undirected;
+
+	@Override
+	public Diff generate(Graph g) {
+		Diff d = new Diff(g.getNodes().length, g.getTimestamp(),
+				g.getTimestamp() + 1);
 
 		while (d.getAddedEdges().size() < add) {
 			Edge e = Rand.edge(g);
+			if (g.containsEdge(e)) {
+				continue;
+			}
 			d.addAddedEdges(e);
 			if (undirected) {
 				d.addAddedEdges(e.invert());
 			}
 		}
 
+		ArrayList<Edge> edges = new ArrayList<Edge>(g.getEdges());
 		while (d.getRemovedEdges().size() < remove) {
-			Edge e = Rand.edge(g);
-			if (d.addsEdge(e)) {
-				continue;
-			}
+			Edge e = edges.get(Rand.rand.nextInt(edges.size()));
 			d.addRemovedEdge(e);
 			if (undirected) {
 				d.addRemovedEdge(e.invert());
--- src/dynamicGraphs/graph/generator/RandomGraph.java
+++ src/dynamicGraphs/graph/generator/RandomGraph.java
@@ -4,16 +4,31 @@ import dynamicGraphs.graph.Edge;
 import dynamicGraphs.graph.Graph;
 import dynamicGraphs.util.Rand;
 
-public class RandomGraph {
-	public static Graph generate(int nodes, int edges, boolean undirected) {
-		Graph g = new Graph("Random Graph", nodes, 0);
-		while (g.getEdges().size() < edges) {
+public class RandomGraph extends GraphGenerator {
+
+	public RandomGraph(int nodes, int edges, boolean undirected) {
+		super("RandomGraph-" + nodes + "-" + edges + "-" + undirected);
+		this.nodes = nodes;
+		this.edges = edges;
+		this.undirected = undirected;
+	}
+
+	private int nodes;
+
+	private int edges;
+
+	private boolean undirected;
+
+	public Graph generate() {
+		Graph g = new Graph("Random Graph", this.nodes, 0);
+		while (g.getEdges().size() < this.edges) {
 			Edge e = Rand.edge(g);
 			g.addEdge(e);
-			if (undirected) {
+			if (this.undirected) {
 				g.addEdge(e.invert());
 			}
 		}
 		return g;
 	}
+
 }
