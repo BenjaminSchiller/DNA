ca7c92d9ae4c6382f846bb3df62919c66a5171a8
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-02-26 14:55:49 +0100

    changes util execute class to throw exception instead of catching them

--- src/dna/util/Execute.java
+++ src/dna/util/Execute.java
@@ -6,36 +6,31 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Execute {
-	public static boolean exec(String cmd) {
-		return Execute.exec(cmd, false);
+	public static void exec(String cmd) throws IOException,
+			InterruptedException {
+		Execute.exec(cmd, false);
 	}
 
-	public static boolean exec(String cmd, boolean printErrors) {
-		return Execute.exec(cmd, printErrors, null);
+	public static void exec(String cmd, boolean printErrors)
+			throws IOException, InterruptedException {
+		Execute.exec(cmd, printErrors, null);
 	}
 
-	public static boolean exec(String cmd, boolean printErrors, String[] env) {
-		try {
-			if (env == null) {
-				env = new String[0];
-			}
-			Process p = Runtime.getRuntime().exec(cmd, env);
-			if (printErrors) {
-				InputStream stderr = p.getErrorStream();
-				InputStreamReader isr = new InputStreamReader(stderr);
-				BufferedReader br = new BufferedReader(isr);
-				String line = null;
-				while ((line = br.readLine()) != null) {
-					System.out.println("err: " + line);
-				}
-			}
-			p.waitFor();
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+	public static void exec(String cmd, boolean printErrors, String[] env)
+			throws IOException, InterruptedException {
+		if (env == null) {
+			env = new String[0];
 		}
-		return false;
+		Process p = Runtime.getRuntime().exec(cmd, env);
+		if (printErrors) {
+			InputStream stderr = p.getErrorStream();
+			InputStreamReader isr = new InputStreamReader(stderr);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println("err: " + line);
+			}
+		}
+		p.waitFor();
 	}
 }
