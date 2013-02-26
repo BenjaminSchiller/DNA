d1b38261527e2af73bda8034a54afe07732817fa
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-02-26 14:58:09 +0100

    adds values datatype

---
+++ src/dna/series/Values.java
@@ -0,0 +1,52 @@
+package dna.series;
+
+import java.io.IOException;
+
+import dna.io.Keywords;
+import dna.io.Suffix;
+import dna.io.Writer;
+
+public class Values {
+	public Values(double[][] values, String name) {
+		this.values = values;
+		this.name = name;
+	}
+
+	private double[][] values;
+
+	public double[][] getValues() {
+		return this.values;
+	}
+
+	private String name;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getFilename() {
+		return this.name + Suffix.values;
+	}
+
+	public void write(String dir) throws IOException {
+		this.write(dir, this.getFilename());
+	}
+
+	public void write(String dir, String filename) throws IOException {
+		Writer writer = new Writer(dir + filename);
+		for (int i = 0; i < this.values.length; i++) {
+			StringBuffer buff = new StringBuffer();
+			for (int j = 0; j < this.values[i].length; j++) {
+				if (j > 0) {
+					buff.append(Keywords.valuesDelimiter);
+				}
+				buff.append(this.values[i][j]);
+				if (this.values[i][j] == Double.NaN) {
+					continue;
+				}
+			}
+			writer.writeln(buff.toString());
+		}
+		writer.close();
+	}
+}
