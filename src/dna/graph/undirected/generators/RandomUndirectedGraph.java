afd23810da4cc789ca81cad7e9c6b8275d6b9785
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-04-30 14:41:03 +0200

    update undirected random graph generator

--- src/dna/graph/undirected/generators/RandomUndirectedGraph.java
+++ src/dna/graph/undirected/generators/RandomUndirectedGraph.java
@@ -1,10 +1,8 @@
 package dna.graph.undirected.generators;
 
-import dna.graph.Graph;
-import dna.graph.GraphDatastructures;
-import dna.graph.Node;
 import dna.graph.undirected.UndirectedEdge;
 import dna.graph.undirected.UndirectedGraph;
+import dna.graph.undirected.UndirectedGraphDatastructures;
 import dna.graph.undirected.UndirectedNode;
 import dna.util.Rand;
 import dna.util.parameters.IntParameter;
@@ -15,36 +13,36 @@ public class RandomUndirectedGraph extends UndirectedGraphGenerator {
 
 	private int edges;
 
-	public RandomUndirectedGraph(int nodes, int edges,
-			GraphDatastructures<UndirectedEdge> datastructures) {
+	public RandomUndirectedGraph(int nodesInit, int nodes, int edgesInit,
+			int edges, long timestampInit,
+			UndirectedGraphDatastructures datastructures) {
 		super("randomUndirectedGraph", new IntParameter("nodes", nodes),
-				new IntParameter("edges", edges), datastructures);
+				new IntParameter("edges", edges), datastructures,
+				timestampInit, nodesInit, edgesInit);
 		this.nodes = nodes;
 		this.edges = edges;
 	}
 
 	@Override
-	public Graph<? extends Node<UndirectedEdge>, UndirectedEdge> generate() {
-		System.out.println(this.datastructures.toString());
+	public UndirectedGraph generate() {
+		UndirectedGraph graph = this.init();
 
-		UndirectedGraph graph = this.getGraphInstance(this.getName(), 0,
-				this.nodes, this.edges);
-		System.out.println("==> " + graph);
 		for (int i = 0; i < this.nodes; i++) {
-			UndirectedNode node = this.getNodeInstance(i);
+			UndirectedNode node = this.datastructures.newNodeInstance(i);
 			graph.addNode(node);
 		}
-		System.out.println("==> " + graph);
+
 		while (graph.getEdgeCount() < this.edges) {
 			int src = Rand.rand.nextInt(graph.getNodeCount());
 			int dst = Rand.rand.nextInt(graph.getNodeCount());
 			if (src != dst) {
-				UndirectedEdge edge = this.getEdgeInstance(graph.getNode(src),
-						graph.getNode(dst));
+				UndirectedEdge edge = this.datastructures.newEdgeInstance(
+						graph.getNode(src), graph.getNode(dst));
 				graph.addEdge(edge);
+				edge.getNode1().addEdge(edge);
+				edge.getNode2().addEdge(edge);
 			}
 		}
-		System.out.println("==> " + graph);
 
 		return graph;
 	}
