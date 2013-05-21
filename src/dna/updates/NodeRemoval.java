e97ae5f634c6fd38f6de6f61dc14161c016fb5a9
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-05-21 17:59:02 +0200

    re-implements node removal in update correctly

--- src/dna/updates/NodeRemoval.java
+++ src/dna/updates/NodeRemoval.java
@@ -3,6 +3,10 @@ package dna.updates;
 import dna.graph.Edge;
 import dna.graph.Graph;
 import dna.graph.Node;
+import dna.graph.directed.DirectedEdge;
+import dna.graph.directed.DirectedNode;
+import dna.graph.undirected.UndirectedEdge;
+import dna.graph.undirected.UndirectedNode;
 import dna.util.Log;
 
 public class NodeRemoval<E extends Edge> extends NodeUpdate<E> {
@@ -19,7 +23,32 @@ public class NodeRemoval<E extends Edge> extends NodeUpdate<E> {
 	@Override
 	public boolean apply(Graph<? extends Node<E>, ? extends E> graph) {
 		Log.debug("=> " + this.toString());
-		return ((Graph<Node<E>, E>) graph).removeNode((Node<E>) this.node);
+		boolean success = true;
+		if (this.node instanceof DirectedNode) {
+			DirectedNode node = (DirectedNode) this.node;
+			for (DirectedEdge e : node.getOutgoingEdges()) {
+				success &= e.getDst().removeEdge(e);
+				success &= ((Graph<Node<E>, E>) graph).removeEdge((E) e);
+			}
+			for (DirectedEdge e : node.getIncomingEdges()) {
+				success &= e.getSrc().removeEdge(e);
+				success &= ((Graph<Node<E>, E>) graph).removeEdge((E) e);
+			}
+		} else if (this.node instanceof UndirectedNode) {
+			UndirectedNode node = (UndirectedNode) this.node;
+			for (UndirectedEdge e : node.getEdges()) {
+				if (node.equals(e.getNode1())) {
+					success &= e.getNode2().removeEdge(e);
+				} else {
+					success &= e.getNode1().removeEdge(e);
+				}
+				success &= ((Graph<Node<E>, E>) graph).removeEdge((E) e);
+			}
+		} else {
+			return false;
+		}
+		success &= ((Graph<Node<E>, E>) graph).removeNode((Node<E>) this.node);
+		return success;
 	}
 
 	@Override
