9c97fc96cf439d5920325581558cbfbd123357a0
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-05-21 13:27:34 +0200

    correctly implements edge updates

--- src/dna/updates/EdgeAddition.java
+++ src/dna/updates/EdgeAddition.java
@@ -3,6 +3,8 @@ package dna.updates;
 import dna.graph.Edge;
 import dna.graph.Graph;
 import dna.graph.Node;
+import dna.graph.directed.DirectedEdge;
+import dna.graph.undirected.UndirectedEdge;
 import dna.util.Log;
 
 public class EdgeAddition<E extends Edge> extends EdgeUpdate<E> {
@@ -19,7 +21,23 @@ public class EdgeAddition<E extends Edge> extends EdgeUpdate<E> {
 	@Override
 	public boolean apply(Graph<? extends Node<E>, ? extends E> graph) {
 		Log.debug("=> " + this.toString());
-		return ((Graph<Node<E>, E>) graph).addEdge((E) this.edge);
+		if (this.edge instanceof DirectedEdge) {
+			boolean success = ((Graph<Node<E>, E>) graph)
+					.addEdge((E) this.edge);
+			DirectedEdge e = (DirectedEdge) this.edge;
+			success &= e.getSrc().addEdge(e);
+			success &= e.getDst().addEdge(e);
+			return success;
+		} else if (this.edge instanceof UndirectedEdge) {
+			boolean success = ((Graph<Node<E>, E>) graph)
+					.addEdge((E) this.edge);
+			UndirectedEdge e = (UndirectedEdge) this.edge;
+			success &= e.getNode1().addEdge(e);
+			success &= e.getNode2().addEdge(e);
+			return success;
+		}
+		Log.error("EdgeAddition - unkown edge type " + this.edge.getClass());
+		return false;
 	}
 
 	@Override
