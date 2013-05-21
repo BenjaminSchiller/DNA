12f7541608922785144d0fa0e3a2dad9e05ae6f4
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-05-21 18:03:41 +0200

    removes old datastructures (diff, metric, graph) and related classes

--- src/dna/util/Rand.java
+++ src/dna/util/Rand.java
@@ -2,9 +2,6 @@ package dna.util;
 
 import java.util.Random;
 
-import dna.graph.old.OldEdge;
-import dna.graph.old.OldGraph;
-
 public class Rand {
 	public static long seed = System.currentTimeMillis();
 
@@ -15,12 +12,4 @@ public class Rand {
 		Rand.rand = new Random(seed);
 	}
 
-	public static OldEdge edge(OldGraph g) {
-		int src = Rand.rand.nextInt(g.getNodes().length);
-		int dst = Rand.rand.nextInt(g.getNodes().length);
-		if (src != dst) {
-			return new OldEdge(g.getNode(src), g.getNode(dst));
-		}
-		return Rand.edge(g);
-	}
 }
