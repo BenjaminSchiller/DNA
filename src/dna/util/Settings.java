b3235a6badc5fb4b2cf89bf5793b5233e2bc3655
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2013-05-27 19:47:31 +0200

    gnuplot path change

--- src/dna/util/Settings.java
+++ src/dna/util/Settings.java
@@ -1,5 +1,6 @@
 package dna.util;
 
 public class Settings {
-	public static final String gnuplotPath = "/usr/bin/gnuplot";
+	//public static final String gnuplotPath = "/usr/bin/gnuplot";	// unix
+	public static final String gnuplotPath = "f:\\!proxx\\gnuplot\\bin\\gnuplot.exe";	// rwilmes windows
 }
