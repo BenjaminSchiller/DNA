2c5bbd6be6e7e52a4436e97bc9c43f5e10d5d247
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2013-06-11 15:22:35 +0200

    adds implementations of timeline batch generators

---
+++ src/dna/updates/directed/DirectedTimelineBatch.java
@@ -0,0 +1,36 @@
+package dna.updates.directed;
+
+import dna.graph.Graph;
+import dna.graph.GraphDatastructures;
+import dna.graph.Node;
+import dna.graph.directed.DirectedEdge;
+import dna.graph.directed.DirectedGraph;
+import dna.graph.directed.DirectedNode;
+import dna.updates.Batch;
+
+public class DirectedTimelineBatch extends DirectedBatchGenerator {
+
+	private DirectedBatchGenerator[] generators;
+
+	private int timestamp;
+
+	public DirectedTimelineBatch(DirectedBatchGenerator[] generators,
+			GraphDatastructures<DirectedGraph, DirectedNode, DirectedEdge> ds) {
+		super("directedTimelineBatch", ds);
+		this.generators = generators;
+		this.timestamp = 0;
+	}
+
+	@Override
+	public Batch<DirectedEdge> generate(
+			Graph<? extends Node<DirectedEdge>, DirectedEdge> graph) {
+		return this.generators[this.timestamp++ % this.generators.length]
+				.generate(graph);
+	}
+
+	@Override
+	public void reset() {
+		this.timestamp = 0;
+	}
+
+}
