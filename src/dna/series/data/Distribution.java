839c1a98af18dd9bfd3b1d1b11c6acd55bdf1d1a
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2013-06-18 10:10:15 +0200

    Codeupdate 13-06-18 Kleinigkeiten im Code angepasst / verbessert sowie Aggregation verfeinert Write-Operation für alle AggregatedData-Objekte implementiert Separate Objekttypen DistributionInt und DistributionLong eingeführt getMin() und getMax() für Distribution-Objekte implementiert
    Signed-off-by: Rwilmes <rene.wilmes@stud.tu-darmstadt.de>

--- src/dna/series/data/Distribution.java
+++ src/dna/series/data/Distribution.java
@@ -12,41 +12,45 @@ import dna.util.Log;
 
 public class Distribution extends Data {
 //public class Distribution implements ListItem {
+
+	// class variables
+	private double[] values;
+	
+	// constructors
 	public Distribution(String name, double[] values) {
-		this.name = name;
+		super(name);
 		this.values = values;
 	}
-
-	public Distribution(String name, double value) {
-		Log.warn("Distribution initialized with a single value");
-		double[] temp = { value };
-		this.values = temp;
-	}
 	
+	//public Distribution() { }
+
+	
+	public Distribution(String name) {
+		super(name);
+	}
+		
+	// class methods
 	public String toString() {
-		return "distribution(" + this.name + ")";
+		return "distribution(" + super.getName() + ")";
 	}
-
-	private String name;
-
-	public String getName() {
-		return this.name;
-	}
-
-	private double[] values;
 
 	public double[] getValues() {
 		return this.values;
 	}
 
-	public String getType() {
-		return "Distribution";
+	public double getValue(int index) {
+		try{
+			return this.values[index];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			Log.error("AggregatedNodeValueList IndexOutOfBoundsException");
+		}
+		return 0;
 	}
 	
 	public void write(String dir, String filename) throws IOException {
 		if (this.values == null) {
 			throw new NullPointerException("no values for distribution \""
-					+ this.name + "\" set to be written to " + dir);
+					+ super.getName() + "\" set to be written to " + dir);
 		}
 		Writer w = new Writer(dir, filename);
 		for (int i = 0; i < this.values.length; i++) {
