1efe54c0630a9f181e8be5ff07488b84c1340d11
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2013-07-04 13:44:35 +0200

    reworked aggregation - added new aggregation objects for a more transparent structure - changed aggregation logic to use the new objects
    Signed-off-by: Rwilmes <rene.wilmes@stud.tu-darmstadt.de>

---
+++ src/dna/series/aggdata/AggregatedDistributionList.java
@@ -0,0 +1,32 @@
+package dna.series.aggdata;
+
+import java.io.IOException;
+
+import dna.io.filesystem.Files;
+import dna.series.lists.List;
+
+/**
+ * An AggregatedDistributionList lists AggregatedDistribution objects.
+ * 
+ * @author Rwilmes
+ * @date 04.07.2013
+ */
+public class AggregatedDistributionList extends List<AggregatedDistribution> {
+
+	// constructors
+	public AggregatedDistributionList() {
+		super();
+	}
+
+	public AggregatedDistributionList(int size) {
+		super(size);
+	}
+	
+	// IO methods
+	public void write(String dir) throws IOException {
+		for (AggregatedDistribution n : this.getList()) {
+			AggregatedData.write(n, dir, Files.getDistributionFilename(n.getName()));
+		}
+	}
+	
+}
--- src/dna/series/aggdata/AggregatedSeries.java
+++ src/dna/series/aggdata/AggregatedSeries.java
@@ -1,91 +1,36 @@
 package dna.series.aggdata;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import dna.io.etc.Keywords;
 import dna.io.filesystem.Dir;
-import dna.io.filesystem.Files;
-import dna.io.filesystem.Names;
-import dna.io.filesystem.Suffix;
 
 /**
  * AggregatedSeries is a class for objects that contain the aggregation for a whole series.
  * 
  * @author Rwilmes
- * @date 25.06.2013
+ * @date 04.07.2013
  */
 public class AggregatedSeries {
 
 	// member variables
-	private HashMap<String, AggregatedDataList>[] aggregation;
+	private AggregatedBatch[] batches;
 	
 	// constructors
 	public AggregatedSeries() {}
-	
-	public AggregatedSeries(HashMap<String, AggregatedDataList>[] aggregation) {
-		this.aggregation = aggregation;
+		
+	public AggregatedSeries(AggregatedBatch[] batches) {
+		this.batches = batches;
 	}
 	
 	// methods
-	public HashMap<String, AggregatedDataList>[] getAggregation() {
-		return this.aggregation;
+	public AggregatedBatch[] getBatches() {
+		return this.batches;
 	}
 	
 	// IO Methods 
 	// TODO: READ ??
 	public void write(String dir) throws IOException {
-		String tempdir = "";
-		HashMap<String, AggregatedDataList>[] aggregation = this.getAggregation();
-		
-		for(int i = 0; i < aggregation.length; i++) {
-			// aggMap contains aggregated data for batch i
-			HashMap<String, AggregatedDataList> aggMap = aggregation[i];
-			tempdir = Dir.getBatchDataDir(dir, i);
-			
-			// each batch got a batchData containing runtime data and statistical values 
-			AggregatedDataList batchStats = aggMap.get(Keywords.batchData);
-			aggMap.remove(Keywords.batchData);
-			if(batchStats != null) {
-				
-				// gathering all statistical values in statsTemp to write at once in one file
-				ArrayList<AggregatedValue> statsTemp = new ArrayList<AggregatedValue>();
-				
-				for(String batchDatas : batchStats.getNames()) {
-					if(batchStats.get(batchDatas) instanceof AggregatedRunTimeList) {
-						AggregatedValue[] aggValuesTemp = ((AggregatedRunTimeList) batchStats.get(batchDatas)).getValues();
-						AggregatedData.write(aggValuesTemp, tempdir, Files.getRuntimesFilename(batchStats.get(batchDatas).getName()));
-					}
-					if(batchStats.get(batchDatas) instanceof AggregatedValue) {
-						statsTemp.add((AggregatedValue) batchStats.get(batchDatas));
-					}
-				}
-				AggregatedData.write( statsTemp , tempdir, Names.batchStats + Suffix.values);
-			}
-
-			// aggMap<metric, AggregatedDataList (of metric)>
-			for(String metric : aggMap.keySet()) {
-				tempdir = Dir.getMetricDataDir(tempdir, metric);
-				AggregatedDataList aggDataList = aggMap.get(metric);
-				
-				ArrayList<AggregatedValue> aggValuesTemp = new ArrayList<AggregatedValue>();
-				
-				for(String aggData : aggDataList.getNames()) {
-					if(aggDataList.get(aggData) instanceof AggregatedDistribution) {
-						AggregatedData.write((AggregatedDistribution) aggDataList.get(aggData), tempdir, Files.getDistributionFilename(aggDataList.get(aggData).getName()));
-					}
-					if(aggDataList.get(aggData) instanceof AggregatedNodeValueList) {
-						AggregatedData.write((AggregatedNodeValueList) aggDataList.get(aggData), tempdir, Files.getNodeValueListFilename(aggDataList.get(aggData).getName()));
-					}
-
-					if(aggDataList.get(aggData) instanceof AggregatedValue) {
-						aggValuesTemp.add((AggregatedValue) aggDataList.get(aggData));
-					}
-				}
-				AggregatedData.write(aggValuesTemp, tempdir, Names.metricDataValues + Suffix.values);
-				tempdir = Dir.getBatchDataDir(dir, i);
-			}
+		for(int i = 0; i < this.getBatches().length; i++) {
+			this.getBatches()[i].write(Dir.getBatchDataDir(dir, i));
 		}
 	}
 		
