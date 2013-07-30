07614d244df4a69d0e5877ff1a225fa0e92e6cda
janwiese <jan.wiese@yahoo.de>
2013-07-30 14:36:41 +0200

    update RCCFirstKNodes.java and RCCPerDegree.java

--- src/dna/metrics/richClubConnectivity/RCCPerDegree.java
+++ src/dna/metrics/richClubConnectivity/RCCPerDegree.java
@@ -1,12 +1,18 @@
 package dna.metrics.richClubConnectivity;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
+import dna.graph.Graph;
+import dna.graph.directed.DirectedEdge;
+import dna.graph.directed.DirectedGraph;
 import dna.graph.directed.DirectedNode;
+import dna.graph.undirected.UndirectedNode;
 import dna.metrics.Metric;
 import dna.series.data.Distribution;
 import dna.series.data.Value;
+import dna.updates.Batch;
 
 @SuppressWarnings("rawtypes")
 // TODO:bei dyn noch bei bisher leeres degree vom vorherigem abziehen
@@ -22,7 +28,7 @@ public abstract class RCCPerDegree extends Metric {
 	}
 
 	@Override
-	public void init() {
+	public void init_() {
 		this.richClubs = new HashMap<Integer, Set<DirectedNode>>();
 		this.richClubCoefficienten = new HashMap<Integer, Double>();
 		this.richClubEdges = new HashMap<Integer, Integer>();
@@ -35,6 +41,50 @@ public abstract class RCCPerDegree extends Metric {
 		this.richClubCoefficienten = new HashMap<Integer, Double>();
 		this.richClubEdges = new HashMap<Integer, Integer>();
 		this.highestDegree = 0;
+	}
+
+	@Override
+	public boolean compute() {
+		DirectedGraph g = (DirectedGraph) this.g;
+		if (DirectedNode.class.isAssignableFrom(this.g.getGraphDatastructures()
+				.getNodeType())) {
+			for (DirectedNode n : g.getNodes()) {
+				int degree = n.getOutDegree();
+				this.highestDegree = Math.max(highestDegree, degree);
+				if (richClubs.containsKey(degree)) {
+					this.richClubs.get(degree).add(n);
+				} else {
+					Set<DirectedNode> temp = new HashSet<DirectedNode>();
+					temp.add(n);
+					this.richClubs.put(degree, temp);
+				}
+			}
+
+			HashSet<DirectedNode> currentrichclub = new HashSet<DirectedNode>();
+			for (int currentDegree : this.richClubs.keySet()) {
+				int edges = 0;
+				for (DirectedNode n : richClubs.get(currentDegree)) {
+
+					for (DirectedEdge ed : n.getOutgoingEdges()) {
+						if (ed.getDst().getOutDegree() >= currentDegree) {
+							edges++;
+						}
+					}
+					for (DirectedEdge ed : n.getIncomingEdges()) {
+						if (ed.getSrc().getOutDegree() >= currentDegree) {
+							edges++;
+						}
+					}
+				}
+				richClubEdges.put(currentDegree, edges);
+				currentrichclub.addAll(richClubs.get(currentDegree));
+			}
+
+			calculateRCC();
+
+			return true;
+		}
+		return false;
 	}
 
 	protected void calculateRCC() {
@@ -83,5 +133,26 @@ public abstract class RCCPerDegree extends Metric {
 		}
 
 		return result;
+	}
+
+	@Override
+	public boolean isComparableTo(Metric m) {
+		return m != null && m instanceof RCCPerDegree;
+	}
+
+	@Override
+	public boolean isApplicable(Graph g) {
+		return DirectedNode.class.isAssignableFrom(g.getGraphDatastructures()
+				.getNodeType())
+				|| UndirectedNode.class.isAssignableFrom(g
+						.getGraphDatastructures().getNodeType());
+	}
+
+	@Override
+	public boolean isApplicable(Batch b) {
+		return DirectedNode.class.isAssignableFrom(b.getGraphDatastructures()
+				.getNodeType())
+				|| UndirectedNode.class.isAssignableFrom(b
+						.getGraphDatastructures().getNodeType());
 	}
 }
--- src/dna/metrics/richClubConnectivity/RCCPerDegreeDyn.java
+++ src/dna/metrics/richClubConnectivity/RCCPerDegreeDyn.java
@@ -1,13 +1,10 @@
 package dna.metrics.richClubConnectivity;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import dna.graph.Graph;
 import dna.graph.directed.DirectedEdge;
 import dna.graph.directed.DirectedNode;
-import dna.metrics.Metric;
 import dna.updates.Batch;
 import dna.updates.EdgeAddition;
 import dna.updates.EdgeRemoval;
@@ -15,6 +12,7 @@ import dna.updates.NodeAddition;
 import dna.updates.NodeRemoval;
 import dna.updates.Update;
 
+@SuppressWarnings("rawtypes")
 public class RCCPerDegreeDyn extends RCCPerDegree {
 
 	public RCCPerDegreeDyn() {
@@ -22,63 +20,17 @@ public class RCCPerDegreeDyn extends RCCPerDegree {
 	}
 
 	@Override
-	public boolean compute() {
-		if (DirectedNode.class.isAssignableFrom(this.g.getGraphDatastructures()
-				.getNodeType())) {
-			for (DirectedNode n : (Collection<DirectedNode>) this.g.getNodes()) {
-				int degree = n.getOutDegree();
-				this.highestDegree = Math.max(highestDegree, degree);
-				if (richClubs.containsKey(degree)) {
-					this.richClubs.get(degree).add(n);
-				} else {
-					Set<DirectedNode> temp = new HashSet<DirectedNode>();
-					temp.add(n);
-					this.richClubs.put(degree, temp);
-				}
-			}
-
-			HashSet<DirectedNode> currentrichclub = new HashSet<DirectedNode>();
-			for (int currentDegree : this.richClubs.keySet()) {
-				int edges = 0;
-				for (DirectedNode n : richClubs.get(currentDegree)) {
-
-					for (DirectedEdge ed : n.getOutgoingEdges()) {
-						if (ed.getDst().getOutDegree() >= currentDegree) {
-							edges++;
-						}
-					}
-					for (DirectedEdge ed : n.getIncomingEdges()) {
-						if (ed.getSrc().getOutDegree() >= currentDegree) {
-							edges++;
-						}
-					}
-				}
-				richClubEdges.put(currentDegree, edges);
-				currentrichclub.addAll(richClubs.get(currentDegree));
-			}
-
-			calculateRCC();
-
-			return true;
-		}
-		return false;
-	}
-
-	@Override
 	public boolean applyBeforeBatch(Batch b) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean applyAfterBatch(Batch b) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean applyBeforeUpdate(Update u) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -240,30 +192,6 @@ public class RCCPerDegreeDyn extends RCCPerDegree {
 		int temp = richClubEdges.get(node.getOutDegree());
 		richClubEdges.put(node.getOutDegree(), temp - updateEdges);
 		return true;
-	}
-
-	@Override
-	protected void init_() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isApplicable(Graph g) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isApplicable(Batch b) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isComparableTo(Metric m) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
