7c74ae4e04a76cbe4a661bce5cfbf39cbc22b435
janwiese <jan.wiese@yahoo.de>
2013-08-07 12:30:08 +0200

    update and error solving cc directed

--- src/dna/metrics/connectedComponents/CCDirected.java
+++ src/dna/metrics/connectedComponents/CCDirected.java
@@ -1,12 +1,11 @@
 package dna.metrics.connectedComponents;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import dna.graph.Graph;
-import dna.graph.Node;
+import dna.graph.directed.DirectedEdge;
 import dna.graph.directed.DirectedGraph;
 import dna.graph.directed.DirectedNode;
 import dna.graph.undirected.UndirectedNode;
@@ -14,21 +13,21 @@ import dna.metrics.Metric;
 import dna.series.data.Distribution;
 import dna.series.data.Value;
 import dna.updates.Batch;
-import dna.util.ArrayUtils;
 
 @SuppressWarnings("rawtypes")
 public abstract class CCDirected extends Metric {
 
-	protected int compCounter;
 	private boolean[] visited;
-	private boolean[] discoverd;
-	protected int[] nodeComponentMembership;
-	protected SpanningTreeNode[] nodesTreeElement;
-	protected Map<Integer, List<Node>> reachableNodesFromComponet;
 
-	// new algorithm
-	protected List<List<ComponentVertex>> components;
-	protected int[] nodeComponentMembership2;
+	// DAGGER
+	private Stack<DirectedNode> s = new Stack<DirectedNode>();
+	private int[] lowLink = new int[this.g.getNodeCount()];
+	private int[] index = new int[this.g.getNodeCount()];
+	private int ind = 0;
+	protected int componentCounter = 0;
+	protected HashMap<Integer, Integer> containmentEdges = new HashMap<>();
+	protected Map<Integer, ComponentVertex> dag = new HashMap<Integer, ComponentVertex>();
+	protected Map<Integer, ComponentVertex> dagExpired = new HashMap<Integer, ComponentVertex>();
 
 	public CCDirected(String name, ApplicationType type) {
 		super(name, type);
@@ -36,72 +35,73 @@ public abstract class CCDirected extends Metric {
 
 	@Override
 	public void init_() {
-		this.nodeComponentMembership = new int[this.g.getNodes().size()];
-		this.nodesTreeElement = new SpanningTreeNode[this.g.getNodes().size()];
-		this.visited = new boolean[this.g.getNodes().size()];
-		this.discoverd = new boolean[this.g.getNodes().size()];
-		this.reachableNodesFromComponet = new HashMap<Integer, List<Node>>();
-		this.compCounter = 0;
+
 	}
 
 	@Override
 	public void reset_() {
-		this.nodeComponentMembership = new int[this.g.getNodes().size()];
-		this.nodesTreeElement = new SpanningTreeNode[this.g.getNodes().size()];
-		this.visited = new boolean[this.g.getNodes().size()];
-		this.discoverd = new boolean[this.g.getNodes().size()];
-		this.reachableNodesFromComponet = new HashMap<Integer, List<Node>>();
-		this.compCounter = 0;
+
 	}
 
 	@Override
 	public boolean compute() {
 		DirectedGraph g = (DirectedGraph) this.g;
+		s.clear();
+		ind = 0;
 		for (DirectedNode n : g.getNodes()) {
 			if (!visited[n.getIndex()]) {
-				this.discoverd = new boolean[this.g.getNodes().size()];
-				bfs(n);
+				tarjan(n);
 			}
 		}
 
 		return true;
 	}
 
-	private void bfs(Node node) {
-		// try {
-		// compCounter++;
-		// int comp = node.getIndex();
-		// Queue q = new Queue();
-		// q.enqueue(new SpanningTreeNode(node));
-		// this.discoverd[node.getIndex()] = true;
-		//
-		// List<Node> reachables = new ArrayList<Node>();
-		// while (!q.isEmpty()) {
-		// SpanningTreeNode temp = (SpanningTreeNode) q.dequeue();
-		// for (Node n : temp.getNode().getOut()) {
-		// if (!this.discoverd[n.getIndex()]) {
-		// this.discoverd[n.getIndex()] = true;
-		// SpanningTreeNode newChild = new SpanningTreeNode(n);
-		// newChild.setParent(temp);
-		// temp.addChild(newChild);
-		// reachables.add(n);
-		// q.enqueue(newChild);
-		//
-		// } else if (!this.visited[n.getIndex()]) {
-		// this.visited[n.getIndex()] = true;
-		// this.nodeComponentMembership[temp.getNode().getIndex()] = comp;
-		// this.nodesTreeElement[temp.getNode().getIndex()] = temp;
-		// reachables.remove(n);
-		// }
-		// }
-		// }
-		//
-		// reachableNodesFromComponet.put(comp, reachables);
-		//
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+	private void tarjan(DirectedNode node) {
+		index[node.getIndex()] = ind;
+		lowLink[node.getIndex()] = ind;
+		visited[node.getIndex()] = true;
+		ind += 1;
+		s.push(node);
+
+		for (DirectedEdge e : node.getOutgoingEdges()) {
+			if (!visited[e.getDst().getIndex()]) {
+				tarjan(e.getDst());
+				lowLink[node.getIndex()] = Math.min(lowLink[node.getIndex()],
+						lowLink[e.getDst().getIndex()]);
+			} else if (s.contains(e.getDst())) {
+				lowLink[node.getIndex()] = Math.min(lowLink[node.getIndex()],
+						lowLink[e.getDst().getIndex()]);
+			}
+		}
+
+		if (index[node.getIndex()] == lowLink[node.getIndex()]) {
+			DirectedNode n;
+			// TODO: nur erzeugen wenn component size groößer gleich 1
+			do {
+				n = s.pop();
+				containmentEdges.put(n.getIndex(), componentCounter);
+				for (DirectedEdge ed : n.getOutgoingEdges()) {
+					if (containmentEdges.containsKey(ed.getDst().getIndex())) {
+						if (containmentEdges.get(ed.getDst().getIndex()) != componentCounter) {
+							// TODO:dAGEdges.add(new DAGEdge(, dst))
+						}
+					}
+				}
+				for (DirectedEdge ed : n.getOutgoingEdges()) {
+					if (containmentEdges.containsKey(ed.getSrc().getIndex())) {
+						if (containmentEdges.get(ed.getSrc().getIndex()) != componentCounter) {
+							// TODO:dAGEdges.add(new DAGEdge(, dst))
+						}
+					}
+				}
+
+			} while (n == node);
+			this.dag.put(this.componentCounter, new ComponentVertex(
+					this.componentCounter));
+			componentCounter++;
+		}
+
 	}
 
 	@Override
@@ -111,45 +111,22 @@ public abstract class CCDirected extends Metric {
 		}
 		CCDirected cc = (CCDirected) m;
 
-		if (!ArrayUtils.equals(this.nodeComponentMembership,
-				cc.getNodeComponentMembership())) {
-			return false;
-		}
+		boolean success = true;
+		success &= this.dag.equals(cc.dag);
 
-		if (this.compCounter != cc.getCompCounter()) {
-			return false;
-		}
-		return true;
+		return success;
 	}
 
 	@Override
 	protected Value[] getValues() {
 		Value v1 = new Value("NumberofComponents", countComponents());
-		Value v2 = new Value("AverageComponentSize",
-				calculateAverageComponentSize());
 
-		return new Value[] { v1, v2 };
+		return new Value[] { v1 };
 	}
 
 	private double countComponents() {
-		HashSet<Integer> compSize = new HashSet<Integer>();
-		for (int i = 0; i < nodeComponentMembership.length; i++) {
-			if (!compSize.contains(this.nodeComponentMembership[i])) {
-				compSize.add(this.nodeComponentMembership[i]);
-			}
-		}
-		return compSize.size();
-	}
 
-	private double calculateAverageComponentSize() {
-		HashSet<Integer> compSize = new HashSet<Integer>();
-		for (int i = 0; i < nodeComponentMembership.length; i++) {
-			if (!compSize.contains(this.nodeComponentMembership[i])) {
-				compSize.add(this.nodeComponentMembership[i]);
-			}
-		}
-
-		return this.g.getNodes().size() / compSize.size();
+		return this.dag.size();
 	}
 
 	@Override
@@ -160,14 +137,14 @@ public abstract class CCDirected extends Metric {
 
 	private double[] calculateComponents() {
 		HashMap<Integer, Integer> sum = new HashMap<Integer, Integer>();
-		for (int i = 0; i < nodeComponentMembership.length; i++) {
-			if (sum.containsKey(nodeComponentMembership[i])) {
-				int temp = sum.get(nodeComponentMembership[i]) + 1;
-				sum.put(nodeComponentMembership[i], temp);
-			} else {
-				sum.put(nodeComponentMembership[i], 1);
-			}
-		}
+		// for (int i = 0; i < nodeComponentMembership.length; i++) {
+		// if (sum.containsKey(nodeComponentMembership[i])) {
+		// int temp = sum.get(nodeComponentMembership[i]) + 1;
+		// sum.put(nodeComponentMembership[i], temp);
+		// } else {
+		// sum.put(nodeComponentMembership[i], 1);
+		// }
+		// }
 		double[] components = new double[sum.size()];
 		int count = 0;
 		for (int s : sum.keySet()) {
@@ -176,14 +153,6 @@ public abstract class CCDirected extends Metric {
 		}
 
 		return components;
-	}
-
-	public int[] getNodeComponentMembership() {
-		return nodeComponentMembership;
-	}
-
-	public int getCompCounter() {
-		return compCounter;
 	}
 
 	@Override
--- src/dna/metrics/connectedComponents/ComponentVertex.java
+++ src/dna/metrics/connectedComponents/ComponentVertex.java
@@ -4,6 +4,23 @@ import java.util.LinkedList;
 
 public class ComponentVertex {
 
-	private LinkedList in;
-	private LinkedList out;
+	public ComponentVertex(int index) {
+		this.index = index;
+	}
+
+	private int index = 0;
+	private LinkedList<DAGEdge> edges = new LinkedList<DAGEdge>();
+
+	public LinkedList<DAGEdge> getEdges() {
+		return edges;
+	}
+
+	public void addEdge(DAGEdge edge) {
+		edges.add(edge);
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
 }
