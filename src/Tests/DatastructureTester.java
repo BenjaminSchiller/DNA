e2253aefe043cf74ac77b97c91cebd5f20d6412c
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-08-21 15:45:33 +0200

    More tests and a hashmap implementation

--- src/Tests/DatastructureTester.java
+++ src/Tests/DatastructureTester.java
@@ -40,14 +40,15 @@ public class DatastructureTester {
 
     @Parameterized.Parameters(name="{0} {1}")
 	public static Collection testPairs() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-    	Class[] dataStructures = {DArrayList.class, DArray.class, DHashSet.class};
+    	Class[] dataStructures = {DArrayList.class, DArray.class, DHashSet.class, DHashMap.class};
 
     	ArrayList<Object> result = new ArrayList<>();
     	for ( Class sD: dataStructures ) {
     		for ( Class sE: elementClasses) {
     			// Check whether we can store an object of sE in sD
-    			DataStructure ds = (DataStructure) sD.getConstructor(Class.class).newInstance(sE);
-    			if ( !ds.canStore(sE)) continue;
+    			if (( Node.class.isAssignableFrom(sE) && !INodeListDatastructure.class.isAssignableFrom(sD) ) ||
+    				( Edge.class.isAssignableFrom(sE) && !IEdgeListDatastructure.class.isAssignableFrom(sD) ))
+    				continue;
     			result.add(new Object[]{sD, sE});
     		}
     	}
@@ -71,21 +72,38 @@ public class DatastructureTester {
     	 */ 
     	
     	for ( Class otherElementClass: elementClasses ) {
-    		if (elementClass.isAssignableFrom(otherElementClass)) exceptionCounter++;
+    		if (elementClass.isAssignableFrom(otherElementClass)) {
+    			assertTrue("Datastructure " + dataStructure.getClass() + "[" + this.elementClass + "] can't store " + otherElementClass + " even if it should", dataStructure.canStore(otherElementClass));
+    			exceptionCounter++;
+    			continue;
+    		}
     		dummy = mock(otherElementClass);
+    		
+    		boolean exceptionThrown = false;
+			assertFalse("Datastructure " + dataStructure.getClass() + "[" + this.elementClass + "/" + dataStructure.getDataType() + "] can store " + otherElementClass + " even if it shouldn't", dataStructure.canStore(otherElementClass));
+    		
     		try {
     			dataStructure.add(dummy);
     		} catch ( RuntimeException e ) {
     			exceptionCounter++;
+    			exceptionThrown = true;
     		}
+    		
+    		assertTrue("Whoopsie - whats up with " + otherElementClass, exceptionThrown);
+    			/*
+    			 * So, this is thrown when the datastructure is not allowed to be added to
+    			 * that list, but no exception is thrown. Common error: add() has not called canAdd()
+    			 */
     	}
     	  	
     	assertEquals(elementClasses.length, exceptionCounter);
     }
 
-	@Test
+	@Test(timeout=1500)
 	public void checkAddAndRemove() {
 		IElement dummy = mock(elementClass);
+		when(dummy.getIndex()).thenReturn(42);
+		
 		assertFalse(dataStructure.contains(dummy));
 		assertEquals(0, dataStructure.size());
 		dataStructure.add(dummy);
@@ -99,6 +117,7 @@ public class DatastructureTester {
 	@Test
 	public void checkMaxNodeIndexOnAddAndRemove() {
 		assumeTrue(dataStructure instanceof INodeListDatastructure);
+		assumeTrue(elementClass.isAssignableFrom(Node.class));
 		INodeListDatastructure tempDS = (INodeListDatastructure) dataStructure;
 		
 		IElement[] dummies = new IElement[10];
@@ -151,6 +170,7 @@ public class DatastructureTester {
 	@Test
 	public void checkGetNode() {
 		assumeTrue(dataStructure instanceof INodeListDatastructure);
+		assumeTrue(elementClass.isAssignableFrom(Node.class));
 		INodeListDatastructure tempDS = (INodeListDatastructure) dataStructure;
 		
 		IElement dummy = mock(this.elementClass);
