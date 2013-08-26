d0eff751880765f6cc511a43ff4b124ad981fc82
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-08-26 16:41:01 +0200

    A little restructoring to do legacy first and new stuff then

--- importExperiments.sh
+++ importExperiments.sh
@@ -9,8 +9,7 @@ function legacy {
 	git mv src/$1 legacy/$1
 }
 
-function replace {
-    git mv src/$1 legacy/$1
+function newversion {
     git mv experimental/src/$1 src/$1
 }
  
@@ -20,7 +19,7 @@ git remote add -f experimental git@github.com:NicoHaase/DNAExperiment.git
 git merge -s ours --no-commit experimental/master
 git read-tree --prefix=experimental/ -u experimental/master
 git commit -m "Merge experimental branch into ours"
- 
+
 echo ""
 echo "Second step: replace former classes with experimental ones"
 
@@ -39,43 +38,50 @@ git mv experimental/src/dna/graph/IWeighted.java src/dna/graph/
  
 mkdirectory legacy
 mkdirectory legacy/dna
- 
 mkdirectory legacy/dna/graph
-replace dna/graph/Graph.java
- 
 mkdirectory legacy/dna/graph/edges
-replace dna/graph/edges/
- 
 mkdirectory legacy/dna/graph/nodes
-replace dna/graph/nodes/
- 
 mkdirectory legacy/dna/graph/directed
-git mv src/dna/graph/directed/* legacy/dna/graph/directed/
- 
 mkdirectory legacy/dna/graph/undirected
-git mv src/dna/graph/undirected/* legacy/dna/graph/undirected/
-
-rmdir experimental/src/dna/graph
-
 mkdirectory legacy/dna/io
-replace dna/io/Reader.java
-replace dna/io/Writer.java
-replace dna/io/GraphReader.java
-replace dna/io/GraphWriter.java
-
 mkdirectory legacy/dna/io/etc
-replace dna/io/etc/Keywords.java
-
-rmdir experimental/src/dna/io/etc
-rmdir experimental/src/dna/io/
-
-git mv experimental/lib/mockit* lib/
  
+git mv src/dna/graph/directed/* legacy/dna/graph/directed/
+git mv src/dna/graph/undirected/* legacy/dna/graph/undirected/
 legacy dna/graph/GraphDatastructures.java 
 legacy dna/graph/GraphGenerator.java
 legacy dna/graph/GraphImpl.java
 legacy dna/graph/WeightedEdge.java
 legacy dna/graph/WeightedNode.java
+
+legacy dna/graph/Graph.java
+legacy dna/graph/edges/
+legacy dna/graph/nodes/
+legacy dna/io/Reader.java
+legacy dna/io/Writer.java
+legacy dna/io/GraphReader.java
+legacy dna/io/GraphWriter.java
+legacy dna/io/etc/Keywords.java
+
+git commit -m "Moved Bennis stuff to legacy section"
+
+echo " "
+echo "Finished moving old stuff, moving new stuff new"
+
+newversion dna/graph/Graph.java
+newversion dna/graph/edges/
+newversion dna/graph/nodes/
+newversion dna/io/Reader.java
+newversion dna/io/Writer.java
+newversion dna/io/GraphReader.java
+newversion dna/io/GraphWriter.java
+newversion dna/io/etc/Keywords.java
+ 
+rmdir experimental/src/dna/graph
+rmdir experimental/src/dna/io/etc
+rmdir experimental/src/dna/io/
+
+git mv experimental/lib/mockit* lib/
  
 git commit -m "Integretated experimental stuff into core"
 
