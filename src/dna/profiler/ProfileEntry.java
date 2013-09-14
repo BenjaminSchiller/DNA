cd39d18a7c71af98ccc2fc15db22bf2d6efae94e
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-09-14 18:25:51 +0200

    Refactoring and more pointcuts

---
+++ src/dna/profiler/ProfileEntry.java
@@ -0,0 +1,35 @@
+package dna.profiler;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+
+import dna.profiler.GraphProfiler.ProfilerType;
+
+public class ProfileEntry {
+	private Map<ProfilerType, Integer> list;
+
+	public ProfileEntry() {
+		list = Collections.synchronizedMap(new EnumMap<ProfilerType, Integer>(ProfilerType.class));
+		for ( ProfilerType p: ProfilerType.values()) {
+			list.put(p, 0);
+		}
+	}	
+	
+	public int get(ProfilerType p) {
+		return list.get(p);
+	}
+	
+	public void increase(ProfilerType p) {
+		list.put(p, list.get(p) + 1);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		for (ProfilerType p : ProfilerType.values()) {
+			s.append("  Calls of type " + p.toString() + ": " + get(p) + "\n");
+		}
+		return s.toString();
+	}
+}
