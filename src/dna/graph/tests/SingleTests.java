4eb1f2e423db5300eacb1a21e32adc0df7552c67
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-09-16 16:21:38 +0200

    Example call to check whether Metric profiles are written correctly

--- src/dna/graph/tests/SingleTests.java
+++ src/dna/graph/tests/SingleTests.java
@@ -3,16 +3,51 @@ package dna.graph.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import dna.graph.IWeighted;
 import dna.graph.datastructures.DArray;
+import dna.graph.datastructures.DArrayList;
 import dna.graph.datastructures.GraphDataStructure;
 import dna.graph.edges.DirectedDoubleWeightedEdge;
+import dna.graph.edges.DirectedEdge;
+import dna.graph.generators.GraphGenerator;
+import dna.graph.generators.RandomGraphGenerator;
 import dna.graph.nodes.DirectedDoubleWeightedNode;
+import dna.graph.nodes.DirectedNode;
+import dna.metrics.Metric;
+import dna.metrics.MetricNotApplicableException;
+import dna.metrics.clusterCoefficient.OpenTriangleClusteringCoefficientUpdate;
+import dna.metrics.degree.DegreeDistributionUpdate;
+import dna.profiler.GraphProfiler;
+import dna.series.AggregationException;
+import dna.series.Series;
+import dna.updates.BatchGenerator;
+import dna.updates.directed.RandomDirectedBatch;
 import dna.util.MathHelper;
+import dna.util.parameters.Parameter;
 
 public class SingleTests {
+	
+	@Test
+	public void metricTest() throws AggregationException, IOException,
+			MetricNotApplicableException {
+		GraphProfiler.activate();
+	
+	GraphDataStructure gds = new GraphDataStructure(DArrayList.class,
+			DArrayList.class, DArrayList.class, DirectedNode.class,
+			DirectedEdge.class);
+	GraphGenerator gg = new RandomGraphGenerator("test",
+			new Parameter[] {}, gds, 1, 40, 40);
+	BatchGenerator<DirectedNode, DirectedEdge> batchGen = new RandomDirectedBatch(
+			5, 5, 5, 5, gds);
+	Metric[] metrics = new Metric[] { new DegreeDistributionUpdate(),
+			new OpenTriangleClusteringCoefficientUpdate() };
+	Series s = new Series(gg, batchGen, metrics, "./graphs/", "test");
+	s.generate(3, 3);
+	}
 
 	@Test
 	public void testMathHelper() {
