b38aa5ab34804bed8bb7e9e724f42c8b738e54f9
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-09-25 14:13:28 +0200

    Performance tests and bug fixes

--- src/dna/graph/tests/SingleTests.java
+++ src/dna/graph/tests/SingleTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import dna.graph.IWeighted;
@@ -13,10 +14,14 @@ import dna.graph.datastructures.DArrayList;
 import dna.graph.datastructures.GraphDataStructure;
 import dna.graph.edges.DirectedDoubleWeightedEdge;
 import dna.graph.edges.DirectedEdge;
+import dna.graph.edges.UndirectedDoubleWeightedEdge;
 import dna.graph.generators.GraphGenerator;
 import dna.graph.generators.RandomGraphGenerator;
 import dna.graph.nodes.DirectedDoubleWeightedNode;
 import dna.graph.nodes.DirectedNode;
+import dna.graph.nodes.IWeightedNode;
+import dna.graph.nodes.UndirectedDoubleWeightedNode;
+import dna.graph.nodes.UndirectedNode;
 import dna.metrics.Metric;
 import dna.metrics.MetricNotApplicableException;
 import dna.metrics.clusterCoefficient.OpenTriangleClusteringCoefficientUpdate;
@@ -27,6 +32,7 @@ import dna.series.Series;
 import dna.updates.BatchGenerator;
 import dna.updates.directed.RandomDirectedBatch;
 import dna.util.MathHelper;
+import dna.util.Timer;
 import dna.util.parameters.Parameter;
 
 public class SingleTests {
@@ -40,13 +46,13 @@ public class SingleTests {
 			DArrayList.class, DArrayList.class, DirectedNode.class,
 			DirectedEdge.class);
 	GraphGenerator gg = new RandomGraphGenerator("test",
-			new Parameter[] {}, gds, 1, 40, 40);
+			new Parameter[] {}, gds, 0, 40, 40);
 	BatchGenerator<DirectedNode, DirectedEdge> batchGen = new RandomDirectedBatch(
 			5, 5, 5, 5, gds);
 	Metric[] metrics = new Metric[] { new DegreeDistributionUpdate(),
 			new OpenTriangleClusteringCoefficientUpdate() };
 	Series s = new Series(gg, batchGen, metrics, "./graphs/", "test");
-	s.generate(3, 3);
+	s.generate(2, 1);
 	}
 
 	@Test
@@ -78,5 +84,54 @@ public class SingleTests {
 			caughtException = true;
 		}
 		assertTrue("Node n2 instantiated with incompatible signature", caughtException);
+	}
+	
+	@Ignore
+	@Test
+	public void performanceTester() {
+		GraphDataStructure gds = new GraphDataStructure(DArray.class,
+				DArray.class, DArray.class, DirectedDoubleWeightedNode.class, DirectedDoubleWeightedEdge.class);
+		
+		int limit = 1000000;
+		IWeightedNode n;
+		
+		Timer t = new Timer("Test");
+		for (int i = 0; i < limit; i++) {
+			n = gds.newWeightedNode(i, 1d);
+		}
+		System.out.println("Generating " + limit + " node instances via gds: " + t.end());
+		
+		t = new Timer("Test");
+		for (int i = 0; i < limit; i++) {
+			n = new DirectedDoubleWeightedNode(i, 1d, gds);
+		}
+		System.out.println("Generating " + limit + " node instances via direct constructor: " + t.end());
+		
+		DirectedDoubleWeightedEdge e;
+		DirectedNode n1 = new DirectedDoubleWeightedNode(1, gds);
+		DirectedDoubleWeightedNode n2 = new DirectedDoubleWeightedNode(2, gds);
+		
+		t = new Timer("Test");
+		for (int i = 0; i < limit; i++) {
+			e = (DirectedDoubleWeightedEdge) gds.newWeightedEdge(n1, n2, 1d);
+		}
+		System.out.println("Generating " + limit + " directed edge instances via gds: " + t.end());
+		
+		t = new Timer("Test");
+		for (int i = 0; i < limit; i++) {
+			e = new DirectedDoubleWeightedEdge(n1, n2, 1d);
+		}
+		System.out.println("Generating " + limit + " edge instances via direct constructor: " + t.end());	
+		
+		gds.setEdgeType(UndirectedDoubleWeightedEdge.class);
+		UndirectedDoubleWeightedEdge ue;
+		UndirectedNode un1 = new UndirectedDoubleWeightedNode(1, gds);
+		UndirectedDoubleWeightedNode un2 = new UndirectedDoubleWeightedNode(2, gds);	
+		
+		t = new Timer("Test");
+		for (int i = 0; i < limit; i++) {
+			ue = (UndirectedDoubleWeightedEdge) gds.newWeightedEdge(un1, un2, 1d);
+		}
+		System.out.println("Generating " + limit + " undirected edge instances via gds: " + t.end());		
 	}
 }
