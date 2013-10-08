ef9cdde47e14ba8a245061e33027b7a78c1cdc21
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2013-10-08 02:11:52 +0200

    Ensure that all data types are tested

--- src/dna/graph/tests/GlobalTestParameters.java
+++ src/dna/graph/tests/GlobalTestParameters.java
@@ -9,15 +9,19 @@ import dna.graph.datastructures.DHashSet;
 import dna.graph.datastructures.DLinkedList;
 import dna.graph.edges.DirectedDoubleWeightedEdge;
 import dna.graph.edges.DirectedEdge;
+import dna.graph.edges.DirectedIntWeightedEdge;
 import dna.graph.edges.UndirectedDoubleWeightedEdge;
 import dna.graph.edges.UndirectedEdge;
+import dna.graph.edges.UndirectedIntWeightedEdge;
 import dna.graph.generators.CliqueGenerator;
 import dna.graph.generators.EmptyGraphGenerator;
 import dna.graph.generators.RandomGraphGenerator;
 import dna.graph.generators.RingGenerator;
 import dna.graph.nodes.DirectedDoubleWeightedNode;
+import dna.graph.nodes.DirectedIntWeightedNode;
 import dna.graph.nodes.DirectedNode;
 import dna.graph.nodes.UndirectedDoubleWeightedNode;
+import dna.graph.nodes.UndirectedIntWeightedNode;
 import dna.graph.nodes.UndirectedNode;
 import dna.updates.generators.RandomBatch;
 import dna.updates.generators.RandomEdgeAdditions;
@@ -29,12 +33,14 @@ import dna.updates.generators.RandomNodeWeights;
 
 public class GlobalTestParameters {
 	public static final Class[] nodeTypes = { UndirectedNode.class,
-			UndirectedDoubleWeightedNode.class, DirectedNode.class,
-			DirectedDoubleWeightedNode.class };
+			UndirectedDoubleWeightedNode.class,
+			UndirectedIntWeightedNode.class, DirectedNode.class,
+			DirectedDoubleWeightedNode.class, DirectedIntWeightedNode.class };
 
 	public static final Class[] edgeTypes = { UndirectedEdge.class,
-			UndirectedDoubleWeightedEdge.class, DirectedEdge.class,
-			DirectedDoubleWeightedEdge.class };
+			UndirectedDoubleWeightedEdge.class,
+			UndirectedIntWeightedEdge.class, DirectedEdge.class,
+			DirectedDoubleWeightedEdge.class, DirectedIntWeightedEdge.class };
 
 	public static final Class[] elementClasses = ObjectArrays.concat(nodeTypes,
 			edgeTypes, Class.class);
