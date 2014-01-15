11396dfc0bbb7904cab4fccd3b7b95e2edc202d8
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2014-01-15 23:51:53 +0100

    Add new data structure: DHashArrayList

--- src/dna/graph/tests/GlobalTestParameters.java
+++ src/dna/graph/tests/GlobalTestParameters.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ObjectArrays;
 import dna.graph.datastructures.DArray;
 import dna.graph.datastructures.DArrayList;
 import dna.graph.datastructures.DEmpty;
+import dna.graph.datastructures.DHashArrayList;
 import dna.graph.datastructures.DHashMap;
 import dna.graph.datastructures.DHashSet;
 import dna.graph.datastructures.DHashTable;
@@ -41,7 +42,7 @@ public class GlobalTestParameters {
 			edgeTypes, Class.class);
 
 	public static final Class[] dataStructures = { DArray.class,
-			DArrayList.class, DHashMap.class, DHashSet.class, DHashTable.class, DLinkedList.class, DEmpty.class };
+			DArrayList.class, DHashArrayList.class, DHashMap.class, DHashSet.class, DHashTable.class, DLinkedList.class, DEmpty.class };
 
 	public static final Class[] graphGenerators = { RandomGraphGenerator.class,
 			CliqueGenerator.class, RingGenerator.class,
