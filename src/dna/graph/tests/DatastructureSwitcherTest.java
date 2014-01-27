3a858ebabd9a1573a10a897823078f8cb700f4a7
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2014-01-27 12:15:19 +0100

    Some more tests for the data structure switching part

--- src/dna/graph/tests/DatastructureSwitcherTest.java
+++ src/dna/graph/tests/DatastructureSwitcherTest.java
@@ -2,8 +2,10 @@ package dna.graph.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Mockito.mock;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,11 +14,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import dna.graph.Graph;
+import dna.graph.IElement;
+import dna.graph.datastructures.DArray;
 import dna.graph.datastructures.DEmpty;
 import dna.graph.datastructures.DataStructure.ListType;
+import dna.graph.datastructures.GraphDataStructure;
 import dna.graph.datastructures.IDataStructure;
+import dna.graph.datastructures.IEdgeListDatastructure;
+import dna.graph.datastructures.IEdgeListDatastructureReadable;
+import dna.graph.datastructures.INodeListDatastructure;
 import dna.graph.datastructures.INodeListDatastructureReadable;
 import dna.graph.datastructures.IReadable;
+import dna.graph.edges.DirectedEdge;
+import dna.graph.generators.GraphGenerator;
+import dna.graph.generators.RandomGraphGenerator;
 import dna.graph.nodes.DirectedNode;
 
 @RunWith(Parameterized.class)
@@ -49,15 +61,16 @@ public class DatastructureSwitcherTest {
 
 	@Test
 	public void checkSimpleConversion() {
-		IReadable oldDSInstance = (IReadable) this.instantiate(oldDS);
+		IReadable oldDSInstance = (IReadable) this.instantiate(
+				ListType.GlobalNodeList, oldDS);
 		DirectedNode n = mock(DirectedNode.class);
 		oldDSInstance.add(n);
 		assertTrue(oldDSInstance.contains(n));
 		assertEquals(1, oldDSInstance.size());
 		assertTrue(oldDSInstance.getClass().equals(oldDS));
 
-		IDataStructure newDSInstance = oldDSInstance
-				.switchTo(instantiate(newDS));
+		IDataStructure newDSInstance = oldDSInstance.switchTo(instantiate(
+				ListType.GlobalNodeList, newDS));
 		assertTrue(newDSInstance.getClass().equals(newDS));
 		if (!newDS.equals(DEmpty.class)) {
 			assertTrue(newDSInstance.contains(n));
@@ -65,21 +78,93 @@ public class DatastructureSwitcherTest {
 		}
 	}
 
-	private IDataStructure instantiate(Class<? extends IDataStructure> ds) {
+	@SuppressWarnings("unchecked")
+	@Test
+	public void checkChangeOfGlobalEdgeList() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		assumeTrue(IEdgeListDatastructureReadable.class.isAssignableFrom(oldDS));
+		assumeTrue(IEdgeListDatastructure.class.isAssignableFrom(newDS));
+
+		GraphDataStructure gdsOld = new GraphDataStructure(DArray.class,
+				(Class<? extends IEdgeListDatastructure>) oldDS, DArray.class,
+				DirectedNode.class, DirectedEdge.class);
+		GraphGenerator gg = new RandomGraphGenerator(gdsOld, 200, 100);
+		Graph g = gg.generate();
+
+		Field innerGlobalEdgeList = Graph.class.getDeclaredField("edges");
+		innerGlobalEdgeList.setAccessible(true);
+		IEdgeListDatastructure listOld = (IEdgeListDatastructure) innerGlobalEdgeList
+				.get(g);
+		assertTrue(listOld.getClass().equals(oldDS));
+
+		int edgesInGraph = g.getEdgeCount();
+
+		g.switchDataStructure(ListType.GlobalEdgeList,
+				instantiate(ListType.GlobalEdgeList, newDS));
+		listOld = (IEdgeListDatastructure) innerGlobalEdgeList.get(g);
+		assertTrue(listOld.getClass().equals(newDS));
+		if (!newDS.equals(DEmpty.class)) {
+			assertEquals(edgesInGraph, listOld.size());
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void checkChangeOfGlobalNodeList() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		assumeTrue(INodeListDatastructureReadable.class.isAssignableFrom(oldDS));
+		assumeTrue(INodeListDatastructure.class.isAssignableFrom(newDS));
+
+		GraphDataStructure gdsOld = new GraphDataStructure(
+				(Class<? extends INodeListDatastructure>) oldDS, DArray.class,
+				DArray.class, DirectedNode.class, DirectedEdge.class);
+		GraphGenerator gg = new RandomGraphGenerator(gdsOld, 200, 100);
+		Graph g = gg.generate();
+
+		Field innerGlobalNodeList = Graph.class.getDeclaredField("nodes");
+		innerGlobalNodeList.setAccessible(true);
+		INodeListDatastructure listOld = (INodeListDatastructure) innerGlobalNodeList
+				.get(g);
+		assertTrue(listOld.getClass().equals(oldDS));
+
+		int nodesInGraph = g.getNodeCount();
+
+		g.switchDataStructure(ListType.GlobalNodeList,
+				instantiate(ListType.GlobalNodeList, newDS));
+		listOld = (INodeListDatastructure) innerGlobalNodeList.get(g);
+		assertTrue(listOld.getClass().equals(newDS));
+		if (!newDS.equals(DEmpty.class)) {
+			assertEquals(nodesInGraph, listOld.size());
+		}
+	}
+
+	private IDataStructure instantiate(ListType lt,
+			Class<? extends IDataStructure> ds) {
+
+		Class<? extends IElement> storedElementClass = null;
+		switch (lt) {
+		case GlobalEdgeList:
+		case LocalEdgeList:
+			storedElementClass = DirectedEdge.class;
+			break;
+		case GlobalNodeList:
+		case LocalNodeList:
+			storedElementClass = DirectedNode.class;
+			break;
+		}
+
 		IDataStructure res = null;
 		try {
 			res = ds.getConstructor(ListType.class, Class.class).newInstance(
-					ListType.GlobalNodeList, DirectedNode.class);
+					lt, storedElementClass);
 		} catch (InstantiationException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException e) {
 			try {
-				res = ds.getConstructor(ListType.class).newInstance(
-						ListType.GlobalNodeList);
+				res = ds.getConstructor(ListType.class).newInstance(lt);
 			} catch (InstantiationException | IllegalAccessException
 					| IllegalArgumentException | InvocationTargetException
 					| NoSuchMethodException | SecurityException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}
