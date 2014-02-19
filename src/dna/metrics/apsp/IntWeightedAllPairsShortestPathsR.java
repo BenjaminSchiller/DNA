a18e7c22934fc78fa7ebf872235657612383e3b8
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2014-02-19 09:28:05 +0100

    fixes a bug in the InWeighted APSP implementation

--- src/dna/metrics/apsp/IntWeightedAllPairsShortestPathsR.java
+++ src/dna/metrics/apsp/IntWeightedAllPairsShortestPathsR.java
@@ -1,10 +1,12 @@
 package dna.metrics.apsp;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 import dna.graph.IElement;
+import dna.graph.edges.DirectedEdge;
 import dna.graph.edges.DirectedIntWeightedEdge;
+import dna.graph.edges.UndirectedEdge;
 import dna.graph.edges.UndirectedIntWeightedEdge;
 import dna.graph.nodes.DirectedNode;
 import dna.graph.nodes.Node;
@@ -40,123 +42,206 @@ public class IntWeightedAllPairsShortestPathsR extends
 		return false;
 	}
 
-	// @Override
-	// public boolean compute() {
-	// // TODO Auto-generated method stub
-	// return false;
-	// }
-
-	protected HashMap<Node, HashMap<Node, Node>> parents;
-
-	protected HashMap<Node, HashMap<Node, Integer>> heights;
-
-	@Override
-	public void init_() {
-		super.init_();
-		this.parents = new HashMap<Node, HashMap<Node, Node>>();
-		this.heights = new HashMap<Node, HashMap<Node, Integer>>();
-	}
-
-	@Override
-	public void reset_() {
-		super.reset_();
-		this.parents = new HashMap<Node, HashMap<Node, Node>>();
-		this.heights = new HashMap<Node, HashMap<Node, Integer>>();
-	}
-
 	@Override
 	public boolean compute() {
 
-		for (IElement ie : g.getNodes()) {
-			Node s = (Node) ie;
+		for (IElement source_ : this.g.getNodes()) {
+			Node source = (Node) source_;
 
-			HashMap<Node, Node> parent = new HashMap<Node, Node>();
-			HashMap<Node, Integer> height = new HashMap<Node, Integer>();
+			int[] dist = this.getInitialDist(this.g.getMaxNodeIndex() + 1);
+			Node[] previous = new Node[this.g.getMaxNodeIndex() + 1];
+			boolean[] visited = new boolean[g.getMaxNodeIndex() + 1];
+			PriorityQueue<Node> Q = new PriorityQueue<Node>(g.getNodeCount(),
+					new DistComparator(dist));
 
-			for (IElement iNode : g.getNodes()) {
-				Node t = (Node) iNode;
-				if (t.equals(s)) {
-					height.put(s, 0);
-				} else {
-					height.put(t, Integer.MAX_VALUE);
+			dist[source.getIndex()] = 0;
+			Q.add(source);
+
+			while (!Q.isEmpty()) {
+				Node current = (Node) Q.remove();
+
+				if (visited[current.getIndex()]) {
+					continue;
 				}
-			}
-			if (DirectedNode.class.isAssignableFrom(this.g
-					.getGraphDatastructures().getNodeType())) {
-				PriorityQueue<QueueElement<DirectedNode>> q = new PriorityQueue<QueueElement<DirectedNode>>();
-				q.add(new QueueElement((DirectedNode) s, height.get(s)));
-				while (!q.isEmpty()) {
-					QueueElement<DirectedNode> c = q.poll();
-					DirectedNode current = c.e;
-					if (height.get(current) == Integer.MAX_VALUE) {
-						break;
+				visited[current.getIndex()] = true;
+
+				if (current instanceof DirectedNode) {
+					for (IElement e : ((DirectedNode) current)
+							.getOutgoingEdges()) {
+						Node n = ((DirectedEdge) e).getDst();
+						this.process(source, current, n,
+								((DirectedIntWeightedEdge) e).getWeight(),
+								dist, previous, visited, Q);
 					}
-
-					for (IElement iEdge : current.getOutgoingEdges()) {
-						DirectedIntWeightedEdge d = (DirectedIntWeightedEdge) iEdge;
-
-						DirectedNode neighbor = d.getDst();
-
-						int alt = height.get(current) + d.getWeight();
-						if (alt < 0) {
-							continue;
-						}
-						if (alt < height.get(neighbor)) {
-							height.put(neighbor, alt);
-							parent.put(neighbor, current);
-							QueueElement<DirectedNode> temp = new QueueElement<DirectedNode>(
-									neighbor, height.get(neighbor));
-							if (q.contains(temp)) {
-								q.remove(temp);
-							}
-							q.add(temp);
-						}
-					}
-				}
-			} else {
-				PriorityQueue<QueueElement<UndirectedNode>> q = new PriorityQueue<QueueElement<UndirectedNode>>();
-				q.add(new QueueElement((UndirectedNode) s, height.get(s)));
-				while (!q.isEmpty()) {
-					QueueElement<UndirectedNode> c = q.poll();
-					UndirectedNode current = c.e;
-
-					if (height.get(current) == Integer.MAX_VALUE) {
-						break;
-					}
-
-					for (IElement iEdge : current.getEdges()) {
-						UndirectedIntWeightedEdge d = (UndirectedIntWeightedEdge) iEdge;
-
-						UndirectedNode neighbor = d.getDifferingNode(current);
-
-						int alt = height.get(current) + d.getWeight();
-						if (alt < 0) {
-							continue;
-						}
-						if (alt < height.get(neighbor)) {
-							height.put(neighbor, alt);
-							parent.put(neighbor, current);
-							QueueElement<UndirectedNode> temp = new QueueElement<UndirectedNode>(
-									neighbor, height.get(neighbor));
-							if (q.contains(temp)) {
-								q.remove(temp);
-							}
-							q.add(temp);
-						}
+				} else if (current instanceof UndirectedNode) {
+					for (IElement e : ((UndirectedNode) current).getEdges()) {
+						Node n = ((UndirectedEdge) e).getDifferingNode(current);
+						this.process(source, current, n,
+								((UndirectedIntWeightedEdge) e).getWeight(),
+								dist, previous, visited, Q);
 					}
 				}
 			}
-			for (int i : height.values()) {
-				if (i != Integer.MAX_VALUE && i != 0) {
-					apsp.incr(i);
+
+			for (int d : dist) {
+				if (d > 0 && d != Integer.MAX_VALUE) {
+					this.apsp.incr(d);
 				}
 			}
-			apsp.truncate();
-			parents.put(s, parent);
-			heights.put(s, height);
 		}
 
 		return true;
 	}
+
+	protected class DistComparator implements Comparator<Node> {
+
+		private int[] dist;
+
+		public DistComparator(int[] dist) {
+			this.dist = dist;
+		}
+
+		@Override
+		public int compare(Node o1, Node o2) {
+			return this.dist[o1.getIndex()] - this.dist[o2.getIndex()];
+		}
+	}
+
+	protected void process(Node source, Node current, Node n, int weight,
+			int[] dist, Node[] previous, boolean[] visited,
+			PriorityQueue<Node> Q) {
+		if (n.getIndex() == source.getIndex()) {
+			return;
+		}
+		int newDist = dist[current.getIndex()] + weight;
+		if (previous[n.getIndex()] == null || newDist < dist[n.getIndex()]) {
+			dist[n.getIndex()] = newDist;
+			previous[n.getIndex()] = current;
+			Q.add(n);
+		}
+	}
+
+	protected int[] getInitialDist(int size) {
+		int[] dist = new int[size];
+		for (int i = 0; i < dist.length; i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
+		return dist;
+	}
+
+	// protected HashMap<Node, HashMap<Node, Node>> parents;
+	//
+	// protected HashMap<Node, HashMap<Node, Integer>> heights;
+	//
+	// @Override
+	// public void init_() {
+	// super.init_();
+	// this.parents = new HashMap<Node, HashMap<Node, Node>>();
+	// this.heights = new HashMap<Node, HashMap<Node, Integer>>();
+	// }
+	//
+	// @Override
+	// public void reset_() {
+	// super.reset_();
+	// this.parents = new HashMap<Node, HashMap<Node, Node>>();
+	// this.heights = new HashMap<Node, HashMap<Node, Integer>>();
+	// }
+	//
+	// @Override
+	// public boolean compute() {
+	//
+	// for (IElement ie : g.getNodes()) {
+	// Node s = (Node) ie;
+	//
+	// HashMap<Node, Node> parent = new HashMap<Node, Node>();
+	// HashMap<Node, Integer> height = new HashMap<Node, Integer>();
+	//
+	// for (IElement iNode : g.getNodes()) {
+	// Node t = (Node) iNode;
+	// if (t.equals(s)) {
+	// height.put(s, 0);
+	// } else {
+	// height.put(t, Integer.MAX_VALUE);
+	// }
+	// }
+	// if (DirectedNode.class.isAssignableFrom(this.g
+	// .getGraphDatastructures().getNodeType())) {
+	// PriorityQueue<QueueElement<DirectedNode>> q = new
+	// PriorityQueue<QueueElement<DirectedNode>>();
+	// q.add(new QueueElement((DirectedNode) s, height.get(s)));
+	// while (!q.isEmpty()) {
+	// QueueElement<DirectedNode> c = q.poll();
+	// DirectedNode current = c.e;
+	// if (height.get(current) == Integer.MAX_VALUE) {
+	// break;
+	// }
+	//
+	// for (IElement iEdge : current.getOutgoingEdges()) {
+	// DirectedIntWeightedEdge d = (DirectedIntWeightedEdge) iEdge;
+	//
+	// DirectedNode neighbor = d.getDst();
+	//
+	// int alt = height.get(current) + d.getWeight();
+	// if (alt < 0) {
+	// continue;
+	// }
+	// if (alt < height.get(neighbor)) {
+	// height.put(neighbor, alt);
+	// parent.put(neighbor, current);
+	// QueueElement<DirectedNode> temp = new QueueElement<DirectedNode>(
+	// neighbor, height.get(neighbor));
+	// if (q.contains(temp)) {
+	// q.remove(temp);
+	// }
+	// q.add(temp);
+	// }
+	// }
+	// }
+	// } else {
+	// PriorityQueue<QueueElement<UndirectedNode>> q = new
+	// PriorityQueue<QueueElement<UndirectedNode>>();
+	// q.add(new QueueElement((UndirectedNode) s, height.get(s)));
+	// while (!q.isEmpty()) {
+	// QueueElement<UndirectedNode> c = q.poll();
+	// UndirectedNode current = c.e;
+	//
+	// if (height.get(current) == Integer.MAX_VALUE) {
+	// break;
+	// }
+	//
+	// for (IElement iEdge : current.getEdges()) {
+	// UndirectedIntWeightedEdge d = (UndirectedIntWeightedEdge) iEdge;
+	//
+	// UndirectedNode neighbor = d.getDifferingNode(current);
+	//
+	// int alt = height.get(current) + d.getWeight();
+	// if (alt < 0) {
+	// continue;
+	// }
+	// if (alt < height.get(neighbor)) {
+	// height.put(neighbor, alt);
+	// parent.put(neighbor, current);
+	// QueueElement<UndirectedNode> temp = new QueueElement<UndirectedNode>(
+	// neighbor, height.get(neighbor));
+	// if (q.contains(temp)) {
+	// q.remove(temp);
+	// }
+	// q.add(temp);
+	// }
+	// }
+	// }
+	// }
+	// for (int i : height.values()) {
+	// if (i != Integer.MAX_VALUE && i != 0) {
+	// apsp.incr(i);
+	// }
+	// }
+	// apsp.truncate();
+	// parents.put(s, parent);
+	// heights.put(s, height);
+	// }
+	//
+	// return true;
+	// }
 
 }
