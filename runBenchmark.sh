68d328d6e5e7ff4e7278de07e4bbba66a1825b50
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2014-03-13 18:01:27 +0100

    Simplified benchmark starter, only relying on DNA.jar created through ANT

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -5,7 +5,7 @@ maxBenchmarkingProcesses=2
 currentlyRunning=$(ps aux|grep java|wc -l)
 maxNumberRunning=$((currentlyRunning+maxBenchmarkingProcesses))
 
-java -cp "lib/*;bin/" dna.profiler.benchmarking.BenchmarkingActions getDS | while read line;
+java -cp DNA.jar dna.profiler.benchmarking.BenchmarkingActions getDS | while read line;
 do
 	# Don't start more processes if there are already enough running!
 	while [ $(ps aux|grep java|wc -l) -ge $maxNumberRunning ];
@@ -14,7 +14,7 @@ do
 	done
 	
 	echo "Starting benchmark for $line"
-	java -cp "lib/*;bin/" dna.profiler.benchmarking.BenchmarkingExperiments $line &
+	java -cp DNA.jar dna.profiler.benchmarking.BenchmarkingExperiments $line &
 	sleep 1
 done
 
@@ -24,4 +24,4 @@ do
 	sleep 5
 done
 
-java -cp "lib/*;bin/" dna.profiler.benchmarking.BenchmarkingActions plot
+java -cp DNA.jar  dna.profiler.benchmarking.BenchmarkingActions plot
