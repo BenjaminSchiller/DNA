9a6e4e423bb077434a3ad4dc1a1c5bc7737591d1
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2014-03-21 12:58:56 +0100

    Move benchmarking configuration to property files

--- src/dna/profiler/benchmarking/BenchmarkingConf.java
+++ src/dna/profiler/benchmarking/BenchmarkingConf.java
@@ -12,9 +12,11 @@ import org.perfidix.meter.Time;
 import org.perfidix.meter.TimeMeter;
 import org.perfidix.ouput.AbstractOutput;
 
+import dna.util.Config;
+
 public class BenchmarkingConf extends AbstractConfig {
 
-	private final static int RUNS = 5;
+	private final static int RUNS = Config.getInt("BENCHMARKING_RUNS");
 	private final static Set<AbstractMeter> METERS = new HashSet<AbstractMeter>();
 	private final static Set<AbstractOutput> OUTPUT = new HashSet<AbstractOutput>();
 
@@ -34,14 +36,22 @@ public class BenchmarkingConf extends AbstractConfig {
 	 */
 	public BenchmarkingConf() {
 		super(RUNS, METERS, OUTPUT, ARRAN, GCPROB);
-		this.inputSizes = new int[] { 1000, 5000, 10000 };
+
+		String inputSizesString = Config.get("BENCHMARKING_INPUTSIZES");
+		String[] splitted = inputSizesString.split(";");
+
+		this.inputSizes = new int[splitted.length];
+		for (int i = 0; i < splitted.length; i++) {
+			inputSizes[i] = Integer.parseInt(splitted[i]);
+		}
+
 		this.operationSize = 50;
 	}
 
 	public int[] getInputSizes() {
 		return this.inputSizes;
 	}
-	
+
 	public int getOperationSize() {
 		return this.operationSize;
 	}
