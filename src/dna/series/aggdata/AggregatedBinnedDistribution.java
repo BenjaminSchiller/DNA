fab3c6b7665817dc181ea17e0e1524e22c8b4c07
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2014-03-23 02:20:19 +0100

    added binsize variable to aggbin. distributions

--- src/dna/series/aggdata/AggregatedBinnedDistribution.java
+++ src/dna/series/aggdata/AggregatedBinnedDistribution.java
@@ -9,6 +9,8 @@ import dna.util.Config;
 
 public class AggregatedBinnedDistribution extends AggregatedDistribution {
 
+	private double binsize;
+
 	// constructor
 	public AggregatedBinnedDistribution(String name) {
 		super(name);
@@ -16,9 +18,40 @@ public class AggregatedBinnedDistribution extends AggregatedDistribution {
 
 	public AggregatedBinnedDistribution(String name, AggregatedValue[] values) {
 		super(name, values);
+		this.binsize = 1;
+	}
+
+	public AggregatedBinnedDistribution(String name, AggregatedValue[] values,
+			double binsize) {
+		super(name, values);
+		this.binsize = binsize;
 	}
 
 	// IO methods
+	public void write(String dir, String filename) throws IOException {
+		Writer w = Writer.getWriter(dir, filename);
+
+		for (int i = 0; i < super.getValues().length; i++) {
+			String temp = "";
+			for (int j = 0; j < super.getValues()[i].getValues().length; j++) {
+				if (j == 0) {
+					String v = Double
+							.toString(super.getValues()[i].getValues()[j]
+									* this.binsize);
+					temp += v + Config.get("AGGREGATED_DATA_DELIMITER");
+				} else {
+					if (j == super.getValues()[i].getValues().length - 1)
+						temp += super.getValues()[i].getValues()[j];
+					else
+						temp += super.getValues()[i].getValues()[j]
+								+ Config.get("AGGREGATED_DATA_DELIMITER");
+				}
+			}
+			w.writeln(temp);
+		}
+		w.close();
+	}
+
 	/**
 	 * @param dir
 	 *            String which contains the path to the directory the
@@ -65,18 +98,12 @@ public class AggregatedBinnedDistribution extends AggregatedDistribution {
 			double[][] values) throws IOException {
 		Writer w = Writer.getWriter(dir, filename);
 
-		String[] binsizeSplit = Double.toString(binsize).split("\\.");
-		int decimal = binsizeSplit[1].length();
-
 		for (int i = 0; i < values.length; i++) {
 			String temp = "";
 			for (int j = 0; j < values[i].length; j++) {
 				if (j == 0) {
 					String v = Double.toString(values[i][j] * binsize);
-					String[] value = v.split("\\.");
-					String index = value[0] + "."
-							+ value[1].substring(0, decimal);
-					temp += index + Config.get("AGGREGATED_DATA_DELIMITER");
+					temp += v + Config.get("AGGREGATED_DATA_DELIMITER");
 				} else {
 					if (j == values[i].length - 1)
 						temp += values[i][j];
