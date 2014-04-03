1058a1556ceb61652dd9c0f30feef85240ac3ba4
Benedict Jahn <benejahn@web.de>
2014-04-03 12:44:21 +0200

    Update 03.04.2014

--- src/dna/metrics/samplingModularity/SamplingModularityR.java
+++ src/dna/metrics/samplingModularity/SamplingModularityR.java
@@ -1,5 +1,14 @@
 package dna.metrics.samplingModularity;
 
+import dna.graph.Graph;
+import dna.metrics.Metric;
+import dna.series.data.Distribution;
+import dna.series.data.NodeNodeValueList;
+import dna.series.data.NodeValueList;
+import dna.series.data.Value;
+import dna.updates.batch.Batch;
+import dna.updates.update.Update;
+
 /**
  * @author Benedict Jahn
  *
@@ -11,6 +20,96 @@ public class SamplingModularityR extends SamplingModularity {
 	 */
 	public SamplingModularityR() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean applyBeforeBatch(Batch b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean applyAfterBatch(Batch b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean applyBeforeUpdate(Update u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean applyAfterUpdate(Update u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean compute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init_() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset_() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Value[] getValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Distribution[] getDistributions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NodeValueList[] getNodeValueLists() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NodeNodeValueList[] getNodeNodeValueLists() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Metric m) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isApplicable(Graph g) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isApplicable(Batch b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isComparableTo(Metric m) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
