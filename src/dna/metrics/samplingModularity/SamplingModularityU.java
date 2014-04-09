93ade15154f9c7a0d89f12aca848cefb32ab0194
Benedict Jahn <benejahn@web.de>
2014-04-09 18:19:03 +0200

    Sampling Modularity reworked. Added undirected option for the SNAP graph reader.

--- src/dna/metrics/samplingModularity/SamplingModularityU.java
+++ src/dna/metrics/samplingModularity/SamplingModularityU.java
@@ -1,114 +1,47 @@
 package dna.metrics.samplingModularity;
 
 import dna.graph.Graph;
-import dna.metrics.Metric;
-import dna.series.data.Distribution;
-import dna.series.data.NodeNodeValueList;
-import dna.series.data.NodeValueList;
-import dna.series.data.Value;
+import dna.graph.nodes.Node;
 import dna.updates.batch.Batch;
+import dna.updates.update.NodeAddition;
 import dna.updates.update.Update;
 
 /**
  * @author Benedict Jahn
- *
+ * 
  */
 public class SamplingModularityU extends SamplingModularity {
 
 	/**
 	 * 
 	 */
-	public SamplingModularityU() {
-		// TODO Auto-generated constructor stub
+	public SamplingModularityU(Graph fullGraph) {
+		super("SamplingModularityU", ApplicationType.AfterBatch,
+				MetricType.exact, fullGraph);
 	}
 
 	@Override
 	public boolean applyBeforeBatch(Batch b) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean applyAfterBatch(Batch b) {
-		// TODO Auto-generated method stub
-		return false;
+		edgesInSample += b.getEdgeAdditionsCount();
+		Iterable<NodeAddition> iter = b.getNodeAdditions();
+		for (NodeAddition na : iter) {
+			degreeSum += getDegreeFromOriginalNode((Node) na.getNode());
+		}
+		return true;
 	}
 
 	@Override
 	public boolean applyBeforeUpdate(Update u) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean applyAfterUpdate(Update u) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean compute() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void init_() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void reset_() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Value[] getValues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Distribution[] getDistributions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public NodeValueList[] getNodeValueLists() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public NodeNodeValueList[] getNodeNodeValueLists() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean equals(Metric m) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isApplicable(Graph g) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isApplicable(Batch b) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isComparableTo(Metric m) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
