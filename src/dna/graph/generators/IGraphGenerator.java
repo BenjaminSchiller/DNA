0928b6a4617ac5b44c86bca724ebd8fae79b6df9
Nico Haase <nico.haase@stud.tu-darmstadt.de>
2014-04-13 13:53:17 +0200

    Remove un-needed checks

--- src/dna/graph/generators/IGraphGenerator.java
+++ src/dna/graph/generators/IGraphGenerator.java
@@ -1,8 +1,6 @@
 package dna.graph.generators;
 
 import dna.graph.Graph;
-import dna.graph.edges.Edge;
-import dna.graph.nodes.Node;
 
 public interface IGraphGenerator {
 	/**
@@ -12,19 +10,4 @@ public interface IGraphGenerator {
 	 */
 	public Graph generate();
 
-	/**
-	 * Check whether a specific node type can be generated using this generator
-	 * 
-	 * @param nodeType
-	 * @return
-	 */
-	public boolean canGenerateNodeType(Class<? extends Node> nodeType);
-	
-	/**
-	 * Check whether a specific edge type can be generated using this generator
-	 * 
-	 * @param nodeType
-	 * @return
-	 */
-	public boolean canGenerateEdgeType(Class<? extends Edge> edgeType);	
 }
