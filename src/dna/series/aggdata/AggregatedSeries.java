e9aa2b3c685bd1a307b04dc87425d46d1d73e395
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2014-04-21 23:43:39 +0200

    PLOTTING: method overhaul + cdf plots

--- src/dna/series/aggdata/AggregatedSeries.java
+++ src/dna/series/aggdata/AggregatedSeries.java
@@ -1,9 +1,12 @@
 package dna.series.aggdata;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import dna.io.filesystem.Dir;
 import dna.series.SeriesGeneration;
+import dna.util.Config;
 
 /**
  * AggregatedSeries is a class for objects that contain the aggregation for a
@@ -45,11 +48,14 @@ public class AggregatedSeries {
 
 	public static AggregatedSeries read(String dir, String name,
 			boolean readValues) throws IOException {
-		String tempDir = Dir.getAggregationDataDir(dir);
+		return read(dir, name, Dir.getBatches(Dir.getAggregationDataDir(dir)),
+				readValues);
+	}
 
-		String[] batches = Dir.getBatches(tempDir);
-		AggregatedBatch[] aggBatches = new AggregatedBatch[Dir
-				.getBatches(tempDir).length];
+	private static AggregatedSeries read(String dir, String name,
+			String[] batches, boolean readValues) throws IOException {
+		String tempDir = Dir.getAggregationDataDir(dir);
+		AggregatedBatch[] aggBatches = new AggregatedBatch[batches.length];
 
 		for (int i = 0; i < batches.length; i++) {
 			long timestamp = Dir.getTimestamp(batches[i]);
@@ -61,7 +67,59 @@ public class AggregatedSeries {
 						Dir.getAggregationBatchDir(dir, timestamp), timestamp,
 						readValues);
 		}
-
 		return new AggregatedSeries(aggBatches);
+	}
+
+	/** Reads only selected batches with a given stepSize **/
+	public static AggregatedSeries readFromTo(String dir, String name,
+			long timestampFrom, long timestampTo, long stepSize,
+			boolean readValues) throws IOException {
+		if (timestampTo == Long.MAX_VALUE && stepSize == 1) {
+			// if read all batches -> use normal method
+			return AggregatedSeries.read(dir, name, readValues);
+		}
+
+		String tempDir = Dir.getAggregationDataDir(dir);
+
+		String[] tempBatches = Dir.getBatches(tempDir);
+		long[] timestamps = new long[tempBatches.length];
+
+		// get timestamps
+		for (int i = 0; i < tempBatches.length; i++) {
+			String[] splits = tempBatches[i].split("\\.");
+			timestamps[i] = Long.parseLong(splits[splits.length - 1]);
+		}
+
+		// sort timestamps
+		Arrays.sort(timestamps);
+
+		// gather relevant batches
+		ArrayList<String> batchesList = new ArrayList<String>();
+		boolean firstBatch = true;
+		int counter = 0;
+		int firstBatchIndex = 0;
+		for (int i = 0; i < timestamps.length; i++) {
+			if (timestamps[i] < timestampFrom || timestamps[i] > timestampTo)
+				continue;
+			if (timestamps[i] >= timestampFrom) {
+				if (firstBatch) {
+					batchesList.add(Config.get("PREFIX_BATCHDATA_DIR")
+							+ timestamps[i]);
+					firstBatch = false;
+					firstBatchIndex = i;
+					counter = 1;
+				} else {
+					long offset = counter * stepSize;
+					if (i == firstBatchIndex + offset) {
+						batchesList.add(Config.get("PREFIX_BATCHDATA_DIR")
+								+ timestamps[i]);
+						counter++;
+					}
+				}
+			}
+		}
+		String[] batches = batchesList.toArray(new String[batchesList.size()]);
+
+		return read(dir, name, batches, readValues);
 	}
 }
