c1e6aed9db4ea3e9dc11ca472caf01e05a1645a4
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2014-05-06 20:29:54 +0200

    adds error messages in case existing edge is added

--- src/dna/updates/update/EdgeAddition.java
+++ src/dna/updates/update/EdgeAddition.java
@@ -7,6 +7,7 @@ import dna.graph.datastructures.GraphDataStructure;
 import dna.graph.edges.Edge;
 import dna.graph.edges.IEdge;
 import dna.graph.nodes.Node;
+import dna.util.Log;
 
 public class EdgeAddition extends EdgeUpdate {
 
@@ -21,9 +22,22 @@ public class EdgeAddition extends EdgeUpdate {
 
 	@Override
 	public boolean apply_(Graph g) {
-		boolean success = g.addEdge((Edge) this.edge);
-		success &= this.edge.connectToNodes();
-		return success;
+		if (!g.addEdge((Edge) this.edge)) {
+			Log.error("could not add edge " + this.edge);
+			if (g.containsEdge((Edge) this.edge)) {
+				Edge old = g.getEdge(this.edge.getN1(), this.edge.getN2());
+				Log.error("edge already exists! " + old + " @ hashes "
+						+ old.hashCode() + " vs. " + this.edge.hashCode()
+						+ " (" + old.equals(this.edge));
+			} else {
+				Log.error("BUT edge not contained in list...");
+			}
+			return false;
+		}
+		if (!this.edge.connectToNodes()) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
