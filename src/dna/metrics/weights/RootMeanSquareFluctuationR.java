db269d7473d92a3f4d521d867d18e45cd5fa39c9
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2014-10-01 17:44:51 +0200

    adds a new version of RootMeanSquareFluctuation

---
+++ src/dna/metrics/weights/RootMeanSquareFluctuationR.java
@@ -0,0 +1,42 @@
+package dna.metrics.weights;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import dna.graph.IElement;
+import dna.graph.nodes.Node;
+import dna.graph.weights.IWeightedNode;
+import dna.metrics.algorithms.IRecomputation;
+import dna.series.data.BinnedDistributionInt;
+import dna.series.data.NodeValueList;
+
+public class RootMeanSquareFluctuationR extends RootMeanSquareFluctuation
+		implements IRecomputation {
+
+	public RootMeanSquareFluctuationR(int steps) {
+		super("RootMeanSquareFluctuationR", MetricType.exact, steps);
+	}
+
+	@Override
+	public boolean recompute() {
+		this.rmsf = new NodeValueList("RootMeanSquareFluctuation", 0);
+		this.rmsfD = new BinnedDistributionInt(
+				"RootMeanSquareFluctuation--Distribution", 0.05);
+
+		if (this.positions == null) {
+			this.positions = new HashMap<Node, LinkedList<double[]>>(
+					this.g.getNodeCount());
+		}
+
+		for (IElement n_ : this.g.getNodes()) {
+			Node n = (Node) n_;
+			LinkedList<double[]> positions = this.update(n,
+					((IWeightedNode) n).getWeight());
+			double rmsf = this.computeRMSF(this.positions.get(n));
+			this.rmsf.setValue(n.getIndex(), rmsf);
+			this.rmsfD.incr(rmsf);
+		}
+
+		return true;
+	}
+}
