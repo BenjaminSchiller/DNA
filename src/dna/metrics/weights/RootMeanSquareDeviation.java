23ab578d0536aa28f223cb1ad9f3759dca52ba6d
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2014-10-01 17:32:15 +0200

    adds new versions of the RootMeanSquareDeviation

---
+++ src/dna/metrics/weights/RootMeanSquareDeviation.java
@@ -0,0 +1,112 @@
+package dna.metrics.weights;
+
+import dna.graph.Graph;
+import dna.graph.weights.Double2dWeight;
+import dna.graph.weights.Double3dWeight;
+import dna.graph.weights.DoubleWeight;
+import dna.graph.weights.IWeightedNode;
+import dna.graph.weights.Int2dWeight;
+import dna.graph.weights.Int3dWeight;
+import dna.graph.weights.IntWeight;
+import dna.metrics.IMetricNew;
+import dna.metrics.MetricNew;
+import dna.series.data.BinnedDistributionInt;
+import dna.series.data.Distribution;
+import dna.series.data.NodeNodeValueList;
+import dna.series.data.NodeValueList;
+import dna.series.data.Value;
+import dna.updates.batch.Batch;
+import dna.util.ArrayUtils;
+import dna.util.DataUtils;
+import dna.util.parameters.Parameter;
+
+/**
+ * 
+ * Root Mean Square Deviation of the position changes of nodes in a
+ * 3-dimensional space. For each node, the difference between their position
+ * from one batch to the other is taken as distance / movement of the node. For
+ * the first snapshot, all nodes are initialize dwith their current position.
+ * Since there is no point of reference to compute the distance to, their
+ * deviation in this first step is 0.
+ * http://en.wikipedia.org/wiki/Root_mean_square_deviation
+ * 
+ * @author benni
+ * 
+ */
+public class RootMeanSquareDeviation extends MetricNew {
+
+	protected int changes;
+
+	protected double rmsd;
+
+	protected BinnedDistributionInt distr;
+
+	public RootMeanSquareDeviation(String name, MetricType metricType,
+			Parameter... p) {
+		super(name, metricType, p);
+	}
+
+	@Override
+	public Value[] getValues() {
+		Value v1 = new Value("RootMeanSquareDeviation", this.rmsd);
+		Value v2 = new Value("Changes", this.changes);
+		return new Value[] { v1, v2 };
+	}
+
+	@Override
+	public Distribution[] getDistributions() {
+		return new Distribution[] { this.distr };
+	}
+
+	@Override
+	public NodeValueList[] getNodeValueLists() {
+		return new NodeValueList[] {};
+	}
+
+	@Override
+	public NodeNodeValueList[] getNodeNodeValueLists() {
+		return new NodeNodeValueList[] {};
+	}
+
+	@Override
+	public boolean isComparableTo(IMetricNew m) {
+		return m != null && m instanceof RootMeanSquareDeviation;
+	}
+
+	@Override
+	public boolean equals(IMetricNew m) {
+		if (m == null || !(m instanceof RootMeanSquareDeviation)) {
+			return false;
+		}
+		RootMeanSquareDeviation m2 = (RootMeanSquareDeviation) m;
+		boolean success = true;
+		success &= DataUtils.equals(this.rmsd, m2.rmsd,
+				"RootMeanSquareDeviation");
+		success &= ArrayUtils.equals(this.distr.getIntValues(),
+				m2.distr.getIntValues(), "DistanceDistribution");
+		return success;
+	}
+
+	@Override
+	public boolean isApplicable(Graph g) {
+		return g.getGraphDatastructures().isNodeType(IWeightedNode.class)
+				&& g.getGraphDatastructures().isNodeWeightType(
+						DoubleWeight.class, Double2dWeight.class,
+						Double3dWeight.class, IntWeight.class,
+						Int2dWeight.class, Int3dWeight.class);
+	}
+
+	@Override
+	public boolean isApplicable(Batch b) {
+		return b.getGraphDatastructures().isNodeType(IWeightedNode.class)
+				&& b.getGraphDatastructures().isNodeWeightType(
+						DoubleWeight.class, Double2dWeight.class,
+						Double3dWeight.class, IntWeight.class,
+						Int2dWeight.class, Int3dWeight.class);
+	}
+
+	protected void initDistr() {
+		this.distr = new BinnedDistributionInt("DistanceDistribution", 0.05);
+	}
+
+}
---
+++ src/dna/metrics/weights/RootMeanSquareDeviationB.java
@@ -0,0 +1,52 @@
+package dna.metrics.weights;
+
+import dna.graph.weights.IWeightedNode;
+import dna.graph.weights.distances.EuclideanDistance;
+import dna.metrics.algorithms.IBeforeBatch;
+import dna.updates.batch.Batch;
+import dna.updates.update.NodeWeight;
+
+public class RootMeanSquareDeviationB extends RootMeanSquareDeviation implements
+		IBeforeBatch {
+
+	public RootMeanSquareDeviationB() {
+		super("RootMeanSquareDeviationB", MetricType.exact);
+	}
+
+	@Override
+	public boolean init() {
+		this.changes = 0;
+		this.rmsd = 0;
+		this.initDistr();
+		for (int i = 0; i < this.g.getNodeCount(); i++) {
+			this.distr.incr(0);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean applyBeforeBatch(Batch b) {
+		this.changes = 0;
+		this.rmsd = 0;
+		this.initDistr();
+
+		int nodesAfter = this.g.getNodeCount() + b.getNodeAdditionsCount()
+				- b.getNodeRemovalsCount();
+		int unchangedNodes = nodesAfter - b.getNodeWeightsCount();
+
+		for (int i = 0; i < unchangedNodes; i++) {
+			this.distr.incr(0);
+		}
+		for (NodeWeight u : b.getNodeWeights()) {
+			double dist = EuclideanDistance.dist(
+					((IWeightedNode) u.getNode()).getWeight(), u.getWeight());
+			this.rmsd += dist * dist;
+			this.distr.incr(dist);
+			this.changes++;
+		}
+		this.rmsd /= this.g.getNodeCount();
+		this.rmsd = Math.sqrt(this.rmsd);
+		return true;
+	}
+
+}
