bf164a574d49893105fd1fa957163d0b4c29e6aa
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2014-11-27 16:01:33 +0100

    adds GrowingRandomEdgeExchange batch generator

---
+++ src/dna/updates/generators/random/GrowingRandomEdgeExchange.java
@@ -0,0 +1,45 @@
+package dna.updates.generators.random;
+
+import dna.graph.Graph;
+import dna.updates.batch.Batch;
+import dna.updates.generators.BatchGenerator;
+import dna.util.parameters.IntParameter;
+
+public class GrowingRandomEdgeExchange extends BatchGenerator {
+
+	private int time;
+
+	private int edges;
+
+	private int maxFails;
+
+	public GrowingRandomEdgeExchange(int edges, int maxFails) {
+		super("GrowingRandomEdgeExchange", new IntParameter("EDGES", edges),
+				new IntParameter("MAX_FAILS", maxFails));
+		this.edges = edges;
+		this.maxFails = maxFails;
+		this.time = 1;
+	}
+
+	@Override
+	public Batch generate(Graph g) {
+		Batch b = this.getCurrentBatch().generate(g);
+		this.time++;
+		return b;
+	}
+
+	private RandomEdgeExchange getCurrentBatch() {
+		return new RandomEdgeExchange(this.time * this.edges, this.maxFails);
+	}
+
+	@Override
+	public void reset() {
+		this.time = 1;
+	}
+
+	@Override
+	public boolean isFurtherBatchPossible(Graph g) {
+		return this.getCurrentBatch().isFurtherBatchPossible(g);
+	}
+
+}
