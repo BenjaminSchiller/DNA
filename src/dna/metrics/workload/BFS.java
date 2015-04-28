e2e9ee8f9e0e5b9c2999d20ae5c8080dfb971e19
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2015-04-28 16:30:51 +0200

    adds BFS and DFS workload operations

---
+++ src/dna/metrics/workload/BFS.java
@@ -0,0 +1,67 @@
+package dna.metrics.workload;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import dna.graph.Graph;
+import dna.graph.IElement;
+import dna.graph.edges.DirectedEdge;
+import dna.graph.edges.UndirectedEdge;
+import dna.graph.nodes.DirectedNode;
+import dna.graph.nodes.Node;
+import dna.graph.nodes.UndirectedNode;
+import dna.util.Log;
+
+/**
+ * 
+ * Workload operation that executes a BFS from a random node until all nodes are
+ * seen (of this node's connected component).
+ * 
+ * @author benni
+ *
+ */
+public class BFS extends RandomElementWorkload {
+
+	public BFS(int times, int samples) {
+		super("BFS", ListType.V, times, samples, 0);
+	}
+
+	@Override
+	protected void createWorkloadE(Graph g) {
+		Log.error("BFSWorkload is not implemented for list type E");
+	}
+
+	@Override
+	protected void createWorkloadV(Graph g) {
+		HashSet<Node> seen = new HashSet<Node>();
+		Node start = this.getSampleNode();
+		seen.add(start);
+		LinkedList<Node> list = new LinkedList<Node>();
+		list.add(start);
+		while (!list.isEmpty()) {
+			Node current = list.removeFirst();
+			if (current instanceof DirectedNode) {
+				for (IElement e : ((DirectedNode) current).getOutgoingEdges()) {
+					Node n = ((DirectedEdge) e).getDst();
+					if (seen.contains(n)) {
+						continue;
+					}
+					seen.add(n);
+					list.add(n);
+				}
+			} else if (current instanceof UndirectedNode) {
+				for (IElement e : ((UndirectedNode) current).getEdges()) {
+					Node n = ((UndirectedEdge) e).getDifferingNode(current);
+					if (seen.contains(n)) {
+						continue;
+					}
+					seen.add(n);
+					list.add(n);
+				}
+			} else {
+				Log.error("unsupported node type: " + current.getClass());
+			}
+		}
+	}
+
+}
