15d573d66aa000f33f2c49358a4bd4dd8a7165f4
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2015-06-26 07:24:08 +0200

    adds some new graph generators (nice for graph vis)

---
+++ src/dna/graph/generators/canonical/Grid3dGraph.java
@@ -0,0 +1,78 @@
+package dna.graph.generators.canonical;
+
+import dna.graph.Graph;
+import dna.graph.datastructures.GraphDataStructure;
+import dna.graph.edges.Edge;
+import dna.graph.generators.GraphGenerator;
+import dna.graph.generators.canonical.HoneyCombGraph.ClosedType;
+import dna.graph.nodes.Node;
+import dna.util.parameters.IntParameter;
+import dna.util.parameters.Parameter;
+import dna.util.parameters.StringParameter;
+
+public class Grid3dGraph extends GraphGenerator {
+
+	private int x;
+	private int y;
+	private int z;
+
+	private ClosedType closedType;
+
+	public Grid3dGraph(GraphDataStructure gds, int x, int y, int z,
+			ClosedType closedType) {
+		super("Grid3dGraph", new Parameter[] { new IntParameter("X", x),
+				new IntParameter("Y", y), new IntParameter("Z", z),
+				new StringParameter("ClosedType", closedType.toString()) },
+				gds, 0, x * y * z, x * y * z * 8);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.closedType = closedType;
+	}
+
+	@Override
+	public Graph generate() {
+		Graph g = this.newGraphInstance();
+
+		Node[][][] nodes = new Node[this.x][this.y][this.z];
+		int index = 0;
+		for (int x = 0; x < this.x; x++) {
+			for (int y = 0; y < this.y; y++) {
+				for (int z = 0; z < this.z; z++) {
+					Node n = this.gds.newNodeInstance(index++);
+					nodes[x][y][z] = n;
+					g.addNode(n);
+					if (x > 0)
+						this.connect(g, nodes[x][y][z], nodes[x - 1][y][z]);
+					if (y > 0)
+						this.connect(g, nodes[x][y][z], nodes[x][y - 1][z]);
+					if (z > 0)
+						this.connect(g, nodes[x][y][z], nodes[x][y][z - 1]);
+				}
+			}
+		}
+
+		if (ClosedType.CLOSED.equals(closedType)) {
+			for (int y = 0; y < this.y; y++) {
+				for (int z = 0; z < this.z; z++) {
+					this.connect(g, nodes[this.x - 1][y][z], nodes[0][y][z]);
+				}
+			}
+		} else if (ClosedType.MOEBIUS.equals(closedType)) {
+			for (int y = 0; y < this.y; y++) {
+				for (int z = 0; z < this.z; z++) {
+					this.connect(g, nodes[this.x - 1][y][z], nodes[0][this.y
+							- y - 1][z]);
+				}
+			}
+		}
+
+		return g;
+	}
+
+	protected void connect(Graph g, Node src, Node dst) {
+		Edge e = this.gds.newEdgeInstance(src, dst);
+		g.addEdge(e);
+		e.connectToNodes();
+	}
+}
---
+++ src/dna/graph/generators/canonical/HoneyCombGraph.java
@@ -0,0 +1,144 @@
+package dna.graph.generators.canonical;
+
+import dna.graph.Graph;
+import dna.graph.datastructures.GraphDataStructure;
+import dna.graph.edges.Edge;
+import dna.graph.generators.GraphGenerator;
+import dna.graph.nodes.Node;
+import dna.util.parameters.IntParameter;
+import dna.util.parameters.Parameter;
+import dna.util.parameters.StringParameter;
+
+public class HoneyCombGraph extends GraphGenerator {
+
+	private int combsX;
+	private int combsY;
+
+	public static enum ClosedType {
+		OPEN, CLOSED, MOEBIUS
+	}
+
+	private ClosedType closedType;
+
+	public HoneyCombGraph(GraphDataStructure gds, int combsX, int combsY,
+			ClosedType closedType) {
+		super("HoneyCombGraph", new Parameter[] {
+				new IntParameter("CombsX", combsX),
+				new IntParameter("CombsY", combsY),
+				new StringParameter("ClosedType", closedType.toString()) },
+				gds, 0, combsY * (combsX * 4 + 2), combsX * combsY * 6);
+		this.combsX = combsX;
+		this.combsY = combsY;
+		this.closedType = closedType;
+	}
+
+	@Override
+	public Graph generate() {
+		Graph g = this.newGraphInstance();
+
+		HoneyComb[][] hcs = new HoneyComb[combsX][combsY];
+		for (int x = 0; x < combsX; x++) {
+			for (int y = 0; y < combsY; y++) {
+				if (y == 0) {
+					hcs[x][y] = new HoneyComb(g, this.gds);
+				} else {
+					hcs[x][y] = new HoneyComb(g, this.gds, hcs[x][y - 1]);
+				}
+				if (x > 0) {
+					this.connect(g, hcs[x - 1][y], hcs[x][y]);
+				}
+			}
+		}
+
+		if (ClosedType.CLOSED.equals(closedType)) {
+			for (int y = 0; y < combsY; y++) {
+				this.connect(g, hcs[combsX - 1][y], hcs[0][y]);
+			}
+		} else if (ClosedType.MOEBIUS.equals(closedType)) {
+			for (int y = 0; y < combsY; y++) {
+				this.connect(g, hcs[combsX - 1][y], hcs[0][combsY - y - 1]);
+			}
+		}
+
+		return g;
+	}
+
+	protected void connect(Graph g, HoneyComb[] row1, HoneyComb[] row2) {
+		for (int i = 0; i < row1.length; i++) {
+			Node src = row1[i].bottom;
+			Node dst = row2[i].top;
+			Edge e = this.gds.newEdgeInstance(src, dst);
+			g.addEdge(e);
+			e.connectToNodes();
+		}
+	}
+
+	protected void connect(Graph g, HoneyComb hc1, HoneyComb hc2) {
+		Edge e = this.gds.newEdgeInstance(hc1.bottom, hc2.top);
+		g.addEdge(e);
+		e.connectToNodes();
+	}
+
+	protected HoneyComb[] createRow(Graph g, GraphDataStructure gds, int count) {
+		HoneyComb[] row = new HoneyComb[count];
+		row[0] = new HoneyComb(g, gds);
+		for (int i = 1; i < row.length; i++) {
+			row[i] = new HoneyComb(g, gds, row[i - 1]);
+		}
+		return row;
+	}
+
+	public static class HoneyComb {
+		public Node top;
+		public Node topLeft;
+		public Node bottomLeft;
+		public Node topRight;
+		public Node bottomRight;
+		public Node bottom;
+
+		public HoneyComb(Graph g, GraphDataStructure gds) {
+			top = addNode(g, gds);
+			topLeft = addNode(g, gds);
+			topRight = addNode(g, gds);
+			bottom = addNode(g, gds);
+			bottomLeft = addNode(g, gds);
+			bottomRight = addNode(g, gds);
+
+			connect(g, gds, top, topLeft);
+			connect(g, gds, top, topRight);
+			connect(g, gds, bottom, bottomLeft);
+			connect(g, gds, bottom, bottomRight);
+			connect(g, gds, topLeft, bottomLeft);
+			connect(g, gds, topRight, bottomRight);
+		}
+
+		public HoneyComb(Graph g, GraphDataStructure gds, HoneyComb leftHC) {
+			top = addNode(g, gds);
+			topLeft = leftHC.topRight;
+			topRight = addNode(g, gds);
+			bottom = addNode(g, gds);
+			bottomLeft = leftHC.bottomRight;
+			bottomRight = addNode(g, gds);
+
+			connect(g, gds, top, topLeft);
+			connect(g, gds, top, topRight);
+			connect(g, gds, bottom, bottomLeft);
+			connect(g, gds, bottom, bottomRight);
+			connect(g, gds, topRight, bottomRight);
+		}
+
+		protected static Node addNode(Graph g, GraphDataStructure gds) {
+			Node n = gds.newNodeInstance(g.getNodeCount());
+			g.addNode(n);
+			return n;
+		}
+
+		protected static void connect(Graph g, GraphDataStructure gds,
+				Node src, Node dst) {
+			Edge e = gds.newEdgeInstance(src, dst);
+			g.addEdge(e);
+			e.connectToNodes();
+		}
+	}
+
+}
