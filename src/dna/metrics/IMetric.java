28c03d9179b8a883352c7bccf41685b5231e5972
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2015-10-14 19:21:17 +0200

    Dist-Integration: IMetric

--- src/dna/metrics/IMetric.java
+++ src/dna/metrics/IMetric.java
@@ -3,7 +3,7 @@ package dna.metrics;
 import dna.graph.Graph;
 import dna.series.data.MetricData;
 import dna.series.data.Value;
-import dna.series.data.distr.Distr;
+import dna.series.data.distr2.Distr;
 import dna.series.data.nodevaluelists.NodeNodeValueList;
 import dna.series.data.nodevaluelists.NodeValueList;
 import dna.updates.batch.Batch;
@@ -47,7 +47,7 @@ public interface IMetric {
 	 * 
 	 * @return all the distributions computed by this metric
 	 */
-	public Distr<?>[] getDistributions();
+	public Distr<?, ?>[] getDistributions();
 
 	/**
 	 * 
