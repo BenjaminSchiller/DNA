f2acb651d5e528dd764250f9cec9b6aeadfa56cc
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-02-19 12:26:04 +0100

    changed isApplicable-functionality in labeler

--- src/dna/labels/labeler/Labeler.java
+++ src/dna/labels/labeler/Labeler.java
@@ -3,10 +3,12 @@ package dna.labels.labeler;
 import java.util.ArrayList;
 
 import dna.graph.Graph;
+import dna.graph.generators.GraphGenerator;
 import dna.labels.Label;
 import dna.metrics.IMetric;
 import dna.series.data.BatchData;
 import dna.updates.batch.Batch;
+import dna.updates.generators.BatchGenerator;
 
 /**
  * Labeller are used to compute labels.
@@ -30,50 +32,10 @@ public abstract class Labeler {
 		return this.name;
 	}
 
-	/**
-	 * Returns if a labeller is applicable/computable given the metrics.
-	 * Depending on the specified labeller-metric-requirement a decision is
-	 * made. <br>
-	 * 
-	 * For this purpose each labeller has to implement its own
-	 * getRequiredMetrics-method, which returns an array of metric-names.
-	 **/
-	public boolean isApplicable(IMetric[] metrics) {
-		String[] reqMetrics = getRequiredMetrics();
-		MetricRequirement req = getRequirementStrategy();
-		boolean[] flags = new boolean[reqMetrics.length];
-
-		// cross-check metrics and set flags
-		for (int i = 0; i < reqMetrics.length; i++) {
-			String reqMetric = reqMetrics[i];
-			for (IMetric m : metrics) {
-				if (m.getName().equals(reqMetric))
-					flags[i] = true;
-			}
-		}
-
-		// check flags and decide
-		for (boolean flag : flags) {
-			if (flag == true && req.equals(MetricRequirement.ATLEAST_ONE))
-				return true;
-			if (flag == false && req.equals(MetricRequirement.ALL))
-				return false;
-		}
-
-		switch (req) {
-		case ATLEAST_ONE:
-			return false;
-		case ALL:
-			return true;
-		default:
-			return false;
-		}
-	}
+	/** Returns if a labeller is applicable/computable given the metrics. **/
+	public abstract boolean isApplicable(GraphGenerator gg, BatchGenerator bg,
+			IMetric[] metrics);
 
 	public abstract ArrayList<Label> computeLabels(Graph g, Batch batch,
 			BatchData batchData, IMetric[] metrics);
-
-	public abstract String[] getRequiredMetrics();
-
-	public abstract MetricRequirement getRequirementStrategy();
 }
