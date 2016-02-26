767fff554c3c07b026dd0060ff1eb46250b8310d
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-02-26 13:17:13 +0100

    renamed run-label-list config-flag

--- src/dna/series/SeriesGeneration.java
+++ src/dna/series/SeriesGeneration.java
@@ -308,7 +308,7 @@ public class SeriesGeneration {
 
 		// write labels to series-labelfile
 		Writer labelListWriter = null;
-		if (write && Config.getBoolean("GENERATION_WRITE_SERIES_LABEL_LIST")) {
+		if (write && Config.getBoolean("GENERATION_WRITE_RUN_LABEL_LISTS")) {
 			labelListWriter = new Writer(series.getDir(),
 					Files.getLabelsListFilename(run));
 			appendLabels(labelListWriter, initialData);
@@ -357,7 +357,7 @@ public class SeriesGeneration {
 
 			// write labels to series-labelfile
 			if (write
-					&& Config.getBoolean("GENERATION_WRITE_SERIES_LABEL_LIST"))
+					&& Config.getBoolean("GENERATION_WRITE_RUN_LABEL_LISTS"))
 				appendLabels(labelListWriter, batchData);
 
 			if (compare) {
