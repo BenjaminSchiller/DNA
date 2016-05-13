46860712239c39b73c03249ae5e050c9771eece1
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2016-05-13 17:00:49 +0200

    adds testing switch operations for gds dynamic

--- src/dna/graph/datastructures/DataStructureReadable.java
+++ src/dna/graph/datastructures/DataStructureReadable.java
@@ -65,9 +65,46 @@ public abstract class DataStructureReadable extends DataStructure implements
 	public IDataStructure switchTo(IDataStructure newDatastructure) {
 		newDatastructure.reinitializeWithSize(this.size());
 		Iterator<IElement> elIt = this.iterator();
-		while ( elIt.hasNext() )
+		while (elIt.hasNext())
 			newDatastructure.add(elIt.next());
 		this.prepareForGC();
 		return newDatastructure;
 	}
+
+	// public IDataStructure switchTo(IDataStructure v2) {
+	// v2.reinitializeWithSize(this.size());
+	// DataStructureReadable v1 = new DArray(this.listType, this.dataType);
+	// v1 = (DataStructureReadable) this.switchTo2(v1);
+	// v2 = ((DataStructureReadable) v1).switchTo2(v2);
+	// this.prepareForGC();
+	// return v2;
+	// }
+	//
+	// public IDataStructure switchTo2(IDataStructure newDatastructure) {
+	// newDatastructure.reinitializeWithSize(this.size());
+	//
+	// if (false && this.dataType.isAssignableFrom(Node.class)) {
+	// ArrayList<Node> sorted = new ArrayList<Node>(this.size());
+	// Iterator<IElement> elIt = this.iterator();
+	// while (elIt.hasNext())
+	// sorted.add((Node) elIt.next());
+	// Collections.sort(sorted, new Comparator<Node>() {
+	// @Override
+	// public int compare(Node n1, Node n2) {
+	// return n1.getIndex() - n2.getIndex();
+	// }
+	// });
+	// for (Node n : sorted) {
+	// newDatastructure.add(n);
+	// }
+	// System.out.println("RESORTING: " + sorted.get(0) + " "
+	// + this.getClass().getSimpleName() + " => "
+	// + newDatastructure.getClass().getSimpleName());
+	// } else {
+	// Iterator<IElement> elIt = this.iterator();
+	// while (elIt.hasNext())
+	// newDatastructure.add(elIt.next());
+	// }
+	// return newDatastructure;
+	// }
 }
