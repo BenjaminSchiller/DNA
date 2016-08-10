a12ec20713bb9aa826033a31ecd99a48f792db3d
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2016-08-10 09:01:40 +0200

    fixes selection of random sources in MSSP

--- src/dna/metrics/paths/unweighted/UnweightedMultiSourceShortestPaths.java
+++ src/dna/metrics/paths/unweighted/UnweightedMultiSourceShortestPaths.java
@@ -34,47 +34,68 @@ public abstract class UnweightedMultiSourceShortestPaths extends
 	}
 
 	protected Iterable<Node> getSources() {
+		// System.out.println("xxx - GETTING SOURCES...");
 		HashSet<Node> all = new HashSet<Node>();
 		for (IElement n : this.getNodesOfAssignedTypes()) {
 			all.add((Node) n);
 		}
+		// System.out.println("xxx - " + all.size() + " nodes to select from");
 
 		if (all.size() == 0) {
+			// System.out.println("xxx - using none!");
 			return new ArrayList<Node>(0);
 		}
 
-		if (all.size() < this.sources) {
-			return all;
-		}
-
-		HashSet<Integer> indexes_ = new HashSet<Integer>();
-		while (indexes_.size() < this.sources) {
-			indexes_.add(Rand.rand.nextInt(all.size()));
-		}
-		int[] indexes = new int[indexes_.size()];
+		Node[] available = new Node[all.size()];
 		int x = 0;
-		for (int index : indexes_) {
-			indexes[x++] = index;
-		}
-
-		// System.out.println("INDEXES: " + indexes.length + " => " + indexes_);
-
-		ArrayList<Node> nodes = new ArrayList<Node>(this.sources);
-		int index = 0;
-		int i = 0;
 		for (Node n : all) {
-			if (index == indexes[i]) {
-				// System.out.println("   ADDING: " + i + " => " + n +
-				// " @ index=" + index);
-				nodes.add(n);
-				i++;
-			}
-			if (i >= indexes.length) {
-				break;
-			}
-			index++;
+			available[x++] = n;
 		}
-		// System.out.println("   => " + nodes.size() + " => " + nodes);
-		return nodes;
+
+		ArrayList<Node> selected = new ArrayList<Node>(this.sources);
+		for (int i = 0; i < this.sources; i++) {
+			selected.add(available[Rand.rand.nextInt(available.length)]);
+		}
+
+		// System.out.println("xxx - selected " + selected.size() + " nodes: " +
+		// selected);
+
+		return selected;
+
+		// if (all.size() < this.sources) {
+		// System.out.println("xxx - using all!");
+		// return all;
+		// }
+		//
+		// HashSet<Integer> indexes_ = new HashSet<Integer>();
+		// while (indexes_.size() < this.sources) {
+		// indexes_.add(Rand.rand.nextInt(all.size()));
+		// }
+		// int[] indexes = new int[indexes_.size()];
+		// int x = 0;
+		// for (int index : indexes_) {
+		// indexes[x++] = index;
+		// }
+		//
+		// // System.out.println("INDEXES: " + indexes.length + " => " +
+		// indexes_);
+		//
+		// ArrayList<Node> nodes = new ArrayList<Node>(this.sources);
+		// int index = 0;
+		// int i = 0;
+		// for (Node n : all) {
+		// if (index == indexes[i]) {
+		// // System.out.println("   ADDING: " + i + " => " + n +
+		// // " @ index=" + index);
+		// nodes.add(n);
+		// i++;
+		// }
+		// if (i >= indexes.length) {
+		// break;
+		// }
+		// index++;
+		// }
+		// System.out.println("xxx -   => " + nodes.size() + " => " + nodes);
+		// return nodes;
 	}
 }
